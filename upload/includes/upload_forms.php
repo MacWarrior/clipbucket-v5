9c9e94c35dea5f9d71e9be306321e5955b5fa8f5
Arslan Hassan <arslan@clip-bucket.com>
2010-01-20 11:58:59 +0000

    REMOVED : php.ini

--- upload/includes/upload_forms.php
+++ upload/includes/upload_forms.php
@@ -8,17 +8,19 @@
 	function load_upload_form($params)
 	{
 		global $file_name;
-		echo '<div class="upload_form_div">';
+		echo '<p>Select video files from your computer to begin uploading.</p>';
+		echo '<div class="upload_form_div clearfix">';
 		echo '<input name="submit_upload" type="hidden" value="Upload Data Now!">';
-		echo '<div class="fieldset flash" id="fsUploadProgress">';
+		echo '<div id="progress_status" class="divStatus">';
 		echo '</div>';
-		echo '<div id="progress_status" class="divStatus"></div><div id="divStatus" class="divStatus">Click \'Upload\' to select files</div>';
-		echo '<div align="right">';
+		echo '<div id="divStatus" class="divStatus moveL">Click \'Upload\' to select files</div>';
+		echo '<div class="moveR">';
 		echo '<span id="spanButtonPlaceHolder"></span>';
 		echo '<input id="btnCancel" type="button" value="Cancel" 
 		onClick="swfu.cancelQueue();" disabled="disabled" style="margin:0px 0px 1px 3px" />';
 		echo '</div>';
 		echo '</div>';
+		echo '<div class="fieldset flash" id="fsUploadProgress"></div>';
 	}
 
 	
@@ -27,7 +29,7 @@
 		global $file_name;
 		if($params['class'])
 			$class = ' '.$params['class'];
-		echo '<div class="upload_form_div'.$class.'">';
+		echo '<div class="upload_form_div clearfix'.$class.'">';
 		echo '<label for="check_url">Please enter remote file url</label><br>';
 		echo '<input type="textfield" id="remote_file_url" name="remote_file_url"  class="upload_input"/>';
 		echo '<input name="submit_upload" type="hidden" value="Upload Data Now!">';
