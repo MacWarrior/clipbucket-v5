e2d6f21ba21c643ae73bbe9c03e04071d6d272a9
Fawaz <fawazcb@users.noreply.github.com>
2010-07-17 08:42:29 +0000

    Added : Re indexing

--- upload/admin_area/reindex_cb.php
+++ upload/admin_area/reindex_cb.php
@@ -20,48 +20,71 @@ $vtbl = tbl("video");
 $utbl = tbl("users");
 $gtbl = tbl("groups");
 
-//Reindex CB Videos
+function p_r($array)
+{
+	echo "<pre>";
+		print_r($array);
+	echo "</pre>";	
+}
+
+//Reindex Videos
 if(isset($_POST['index_vids'])) {
 	$videos = get_videos(array("active"=>"yes","status"=>"Successful"));
 	$total_videos = get_videos(array("count_only"=>true,"active"=>"yes","status"=>"Successful"));
 	$percent = number_format(50 * $total_videos / 100);	
+	$i = 0;
+	
+	while($i < $total_videos) {
+		$params = array("video_id"=>$videos[$i]['videoid'],"video_comments"=>true,"favs_count"=>true,"playlist_count"=>true);
+		$indexes = $cbindex->count_index("vid",$params);
+		$fields = $cbindex->extract_fields("vid",$params);
+		
+		$cbindex->update_index("vid",array("fields"=>$fields,"values"=>$indexes,"video_id"=>$videos[$i]['videoid']));
+		
+		$i++;	
+	}
+	
+	e($total_videos." videos have been reindexed successfully.","m");
 }
 
-//Reindex CB Users
+//Reindex Users
 if(isset($_POST['index_usrs'])) {
 	$users = get_users(array("usr_status"=>"Ok"));
 	$total_users = get_users(array("count_only"=>true,"usr_status"=>"Ok"));
-	$percent = $cbindex->percent(25,$total_users);
-	$cond = '';
+	$percent = $cbindex->percent(50,$total_users);
+	$i = 0;
 	
-	for($i=0;$i<$percent;$i++) {
-		$vparams = array("user"=>$users[$i]['userid'],"video_count"=>true);
-		$video_count = $cbindex->count_index("user",$vparams);
-		if(!empty($cond))
-			$cond .= ", ";
-		$cond .= "$utbl.total_videos = $video_count";	
-			
-		$cparams = array("user"=>$users[$i]['userid'],"comment_count"=>true);
-		$comment_count = $cbindex->count_index("user",$cparams);
-		if(!empty($cond))
-			$cond .= ", ";
-		$cond .= "$utbl.total_comments = $comment_count";
+	while($i < $total_users) {
+		$params = array("user"=>$users[$i]['userid'],"comment_added"=>true,"subscriptions_count"=>true,"subscribers_count"=>true,
+						"video_count"=>true,"groups_count"=>true);
+		$indexes = $cbindex->count_index("user",$params);
+		$fields = $cbindex->extract_fields("user",$params);
+				
+		$cbindex->update_index("user",array("fields"=>$fields,"values"=>$indexes,"user"=>$users[$i]['userid']));
 		
-		$cbindex->update_index("user",array("values"=>$cond,"user"=>$users[$i]['userid']));
-		
-		// After each loop we will empty $cond
-		// Why ? If you dont do this it will
-		// start to concat every cond. 
-		$cond = '';	
-			
+		$i++;	
 	}
 	
-
+	e($total_users." users have been reindexed successfully.","m");
 }
 
-//Reindex CB Grous
+//Reindex Grous
 if(isset($_POST['index_gps'])) {
+	$groups = get_groups(array("active"=>"yes"));
+	$total_groups = get_groups(array("count_only"=>true,"active"=>"yes"));
+	$percent = $cbindex->percent(50,$total_groups);
+	$i = 0;
+
+	while ($i < $total_groups) {
+		$params = array("group_id"=>$groups[$i]['group_id'],"group_videos"=>true,"group_topics"=>true,"group_members"=>true);
+		$indexes = $cbindex->count_index("group",$params);
+		$fields = $cbindex->extract_fields("group",$params);
 		
+		$cbindex->update_index("group",array("fields"=>$fields,"values"=>$indexes,"group_id"=>$groups[$i]['group_id']));
+		
+		$i++;
+	}
+	e($total_groups." groups have been reindexed successfully.","m");
 }
 
 subtitle("Re-index Clipbucket");		
--- upload/includes/classes/reindex.class.php
+++ upload/includes/classes/reindex.class.php
@@ -31,12 +31,15 @@ class CBreindex
 	function count_index($type,$params)
 	{
 		global $db;
-
+		$arr = array();
+		
 		switch($type)
 		{
 			case "user":
 			case "u":
 			{
+				
+				
 				if($params['video_count'])
 				{
 					$video_count = $db->count(tbl($this->vtbl),
@@ -44,21 +47,142 @@ class CBreindex
 											  tbl($this->vtbl).".userid = ".$params['user']." AND 
 											  ".tbl($this->vtbl).".active = 'yes' AND ".tbl($this->vtbl).".status = 'Successful'");
 					//echo $db->db_query;										  
-					return $video_count;						  
+					$arr[] = $video_count;						  
 				}
 										  
-				if($params['comment_count'])
+				if($params['comment_added'])
 				{
 					$ctbl = tbl("comments");
-					$comment_count = $db->count($ctbl,
+					$comment_added = $db->count($ctbl,
 												$ctbl.".comment_id",
 												$ctbl.".userid = ".$params['user']."");	
-					return $comment_count;							
+					$arr[] = $comment_added;							
 				}
+				
+				if($params['comment_received'])
+				{
+					$ctbl = tbl("comments");
+					$comment_received = $db->count($ctbl,
+												$ctbl.".comment_id",
+												$ctbl.".type_id = ".$params['user']." AND
+												".$ctbl.".type = 'c'");	
+					$arr[] = $comment_received;							
+				}
+				
+//				if($params['contacts'])
+//				{
+//					global $userquery;
+//					$contacts = $userquery->get_contacts($params['user'],0,"yes",true);
+//					$arr[] = $contacts;	
+//				}
+//				
+				if($params['groups_count'])
+				{
+					global $cbgroup;
+					$details = array("user"=>$params['user'],"active"=>"yes","count_only"=>true);
+					$groups_count = $cbgroup->get_groups($details);
+					$arr[] = $groups_count;	
+				}
+				
+				// Counting user subscribers
+				if($params['subscribers_count'])
+				{
+					$subtbl = tbl('subscriptions');
+					$subscribers_count = $db->count($subtbl,
+													$subtbl.".subscription_id",
+													$subtbl.".subscribed_to = ".$params['user']."");
+					$arr[] = $subscribers_count;	
+				}
+
+
+				// Counting user subscriptions
+				if($params['subscriptions_count'])
+				{
+					$subtbl = tbl('subscriptions');
+					$subscriptions_count = $db->count($subtbl,
+													$subtbl.".subscription_id",
+													$subtbl.".userid = ".$params['user']."");
+					$arr[] = $subscriptions_count;	
+				}
+										
+				return $arr;
 				
 			}
 			break;
 			
+			case "videos":
+			case "vid":
+			case "v":
+			{
+				//$arr[] = $params['video_id'];
+				if($params['video_comments'])
+				{
+					$ctbl = tbl("comments");
+					$video_comments = $db->count($ctbl,
+												 $ctbl.".comment_id",
+												 $ctbl.".type_id = ".$params['video_id']." AND ".$ctbl.".type = 'v'");
+					$arr[] = $video_comments;							 	
+				}
+				
+				if($params['favs_count'])
+				{
+					$ftbl = tbl("favorites");
+					$favs_count = $db->count($ftbl,
+												 $ftbl.".favorite_id",
+												 $ftbl.".id = ".$params['video_id']." AND ".$ftbl.".type = 'v'");
+					$arr[] = $favs_count;							 	
+				}
+				
+				if($params['playlist_count'])
+				{
+					$ptbl = tbl("playlist_items");
+					$playlist_count = $db->count($ptbl,
+												 $ptbl.".playlist_item_id",
+												 $ptbl.".object_id = ".$params['video_id']." AND ".$ptbl.".playlist_item_type = 'v'");
+					$arr[] = $playlist_count;						 	
+				}
+				
+				return $arr;
+			}
+			break;
+			
+			case "group":
+			case "gp":
+			case "g":
+			{
+				//$arr[] = $params['group_id'];
+				
+				if($params['group_videos'])
+				{
+					$gvtbl = tbl('group_videos');
+					$group_videos = $db->count($gvtbl,
+											   $gvtbl.".group_video_id",
+											   $gvtbl.".group_id = ".$params['group_id']." AND ".$gvtbl.".approved = 'yes'");
+					$arr[] = $group_videos;
+				}
+				
+				if($params['group_topics'])
+				{
+					$gttbl = tbl('group_topics');
+					$group_topics = $db->count($gttbl,
+											   $gttbl.".topic_id",
+											   $gttbl.".group_id = ".$params['group_id']." AND ".$gttbl.".approved = 'yes'");
+					$arr[] = $group_topics;						   
+				}
+				
+				if($params['group_members'])
+				{
+					$gmtbl = tbl('group_members');
+					$group_members = $db->count($gmtbl,
+												$gmtbl.".group_mid",
+												$gmtbl.".group_id = ".$params['group_id']." AND ".$gmtbl.".active = 'yes'");
+												
+					$arr[] = $group_members;							
+				}
+				
+				return $arr;
+			}
+			break;
 		}
 	}
 	
@@ -74,12 +198,119 @@ class CBreindex
 			case "user":
 			case "u":
 			{
-				$test_query = "UPDATE ".tbl($this->utbl)." SET ".$params['values']." WHERE ".tbl($this->utbl).".userid = ".$params['user']."";
-				echo $test_query."<br/ ><br />";				
+				$db->update(tbl($this->utbl),$params['fields'],$params['values'], tbl($this->utbl).".userid = ".$params['user']."");								
+				//echo $db->db_query."<br /><br />";				
 			}
 			break;
 			
+			case "videos":
+			case "vid":
+			case "v":
+			{
+				$db->update(tbl($this->vtbl),$params['fields'],$params['values'], tbl($this->vtbl).".videoid = ".$params['video_id']."");
+				//echo $db->db_query."<br /><br />";	
+			}
+			break;
+			
+			case "group":
+			case "gp":
+			case "g":
+			{
+					$db->update(tbl($this->gtbl),$params['fields'],$params['values'], tbl($this->gtbl).".group_id = ".$params['group_id']."");
+			}
 		}
+	 }
+	 
+	/**
+	 * Function used to extract
+	 * fields
+	 */	
+	 function extract_fields($type,$arr)
+	 {
+		 global $db;
+		 $fields = array();
+		 
+		 switch($type)
+		 {
+			case "user":
+			case "u":
+			{
+				if(is_array($arr))
+				{
+					if(array_key_exists('video_count',$arr))
+						$fields[] = 'total_videos';
+						
+					if(array_key_exists('comment_added',$arr))
+						$fields[] = 'total_comments';
+						
+					if(array_key_exists('comment_received',$arr))
+						$fields[] = 'comments_count';
+					
+					if(array_key_exists('groups_count',$arr))
+						$fields[] = 'total_groups';
+						
+					if(array_key_exists('subscribers_count',$arr))
+						$fields[] = 'subscribers';
+						
+					if(array_key_exists('subscriptions_count',$arr))
+						$fields[] = 'total_subscriptions';						
+					$result  = $fields;					
+				} else {
+					$result = $arr;	
+				}
+				
+				return $result;
+			}
+			break;
+			
+			case "videos":
+			case "vid":
+			case "v":
+			{
+				if(is_array($arr))
+				{
+					if(array_key_exists('video_comments',$arr))
+						$fields[] = "comments_count";
+						
+					if(array_key_exists('favs_count',$arr))
+						$fields[] = "favourite_count";
+						
+					if(array_key_exists('playlist_count',$arr))
+						$fields[] = "playlist_count";	
+					
+					$result  = $fields;			
+				} else {
+					$result = $arr;
+				}
+				
+				return $result;
+			}
+			break;
+			
+			case "group":
+			case "gp":
+			case "g":
+			{
+				if(is_array($arr))
+				{
+					if(array_key_exists('group_videos',$arr))
+						$fields[] = "total_videos";
+						
+					if(array_key_exists('group_topics',$arr))
+						$fields[] = "total_topics";
+
+					if(array_key_exists('group_members',$arr))
+						$fields[] = "total_members";
+
+					$result = $fields;
+					
+				} else {
+					$result = $arr;
+				}
+				
+				return $result;
+			}
+		 }
 	 }
 }
 
