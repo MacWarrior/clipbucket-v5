9a4da7297ca3de82f096e803ea86b976c8cd9179
Fawaz <fawazcb@users.noreply.github.com>
2010-07-27 11:58:34 +0000

    Fixed : Viewing of topics according to post_type field

--- upload/view_topic.php
+++ upload/view_topic.php
@@ -16,11 +16,19 @@ $tdetails = $cbgroup->get_topic_details($tid);
 if($tdetails)
 {
 	$grp_details = $cbgroup->get_group_details($tdetails['group_id']);
-	assign('group',$grp_details);
-	assign('topic',$tdetails);
-	subtitle($grp_details['group_name']);
-	//Calling all functions when a topic is called
-	call_view_topic_functions($tdetails);
+	if($grp_details['post_type'] == 1 && !$cbgroup->is_member(userid(),$grp_details['group_id'],TRUE)) {
+		if($cbgroup->is_member(userid(),$grp_details['group_id']))
+			e(lang("You are still unactive. Please contact group owner if you think a handsome ammount of time have passed since you joined."));			
+		else
+			e(lang("To view topics, please join group."));
+		$Cbucket->show_page = false;
+	} else {
+		assign('group',$grp_details);
+		assign('topic',$tdetails);
+		subtitle($grp_details['group_name']);
+		//Calling all functions when a topic is called
+		call_view_topic_functions($tdetails);
+	}
 }
 
 template_files('view_topic.html');
