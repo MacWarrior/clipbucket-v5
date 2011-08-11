3f7297bd59bbff4c41c781226eb7e2be4c7c3b58
Arslan Hassan <arslan@clip-bucket.com>
2011-08-11 09:36:59 +0000

    Added : counters table in installation files

--- upload/cb_install/sql/structure.sql
+++ upload/cb_install/sql/structure.sql
@@ -1063,3 +1063,14 @@ ALTER TABLE `{tbl_prefix}groups` ADD `group_admins` TEXT NOT NULL AFTER `userid`
 -- Alterations for 2.6
 ALTER TABLE  `{tbl_prefix}video` CHANGE  `category`  `category` VARCHAR( 200 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT  '0';
 ALTER TABLE `{tbl_prefix}collections` CHANGE `category` `category` VARCHAR( 200 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ;
+
+CREATE TABLE IF NOT EXISTS `{tbl_prefix}counters` (
+  `counter_id` int(100) NOT NULL AUTO_INCREMENT,
+  `section` varchar(200) NOT NULL,
+  `query` text NOT NULL,
+  `query_md5` varchar(200) NOT NULL,
+  `counts` bigint(200) NOT NULL,
+  `date_added` varchar(200) NOT NULL,
+  PRIMARY KEY (`counter_id`),
+  UNIQUE KEY `query_md5` (`query_md5`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
--- upload/cb_install/sql/upgrade_2.6.sql
+++ upload/cb_install/sql/upgrade_2.6.sql
@@ -5,3 +5,14 @@ INSERT INTO `{tbl_prefix}config` (`configid`, `name`, `value`) VALUES (NULL, 'se
 
 INSERT INTO `{tbl_prefix}config` (`configid`, `name`, `value`) VALUES (NULL, 'use_cached_pagin', 'yes'),
 (NULL, 'cached_pagin_time', '5');
+
+CREATE TABLE IF NOT EXISTS `{tbl_prefix}counters` (
+  `counter_id` int(100) NOT NULL AUTO_INCREMENT,
+  `section` varchar(200) NOT NULL,
+  `query` text NOT NULL,
+  `query_md5` varchar(200) NOT NULL,
+  `counts` bigint(200) NOT NULL,
+  `date_added` varchar(200) NOT NULL,
+  PRIMARY KEY (`counter_id`),
+  UNIQUE KEY `query_md5` (`query_md5`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
