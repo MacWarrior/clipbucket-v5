ad4cee45cdcd5ddcc3e6c38cfe66e807e2922ae0
Arslan Hassan <arslan@clip-bucket.com>
2011-09-05 11:39:05 +0000

    Fixed : SEO URL redirect for Videos

--- upload/watch_video.php
+++ upload/watch_video.php
@@ -19,6 +19,35 @@ $vdo = $cbvid->get_video($vkey);
 assign('vdo',$vdo);
 if(video_playable($vdo))
 {	
+	
+	/**
+	 * Please check http://code.google.com/p/clipbucket/issues/detail?id=168
+	 * for more details about following code
+	 */
+	 
+	if(SEO=='yes')
+	{
+		//Checking if Video URL is Exactly What we have created
+		$vid_link = videoLink($vdo);
+		$vid_link_seo = explode('/',$vid_link);
+		$vid_link_seo = $vid_link_seo[count($vid_link_seo) -1 ];
+		
+		//What we are getting
+		$server_link = $_SERVER['REQUEST_URI'];
+		$server_link_seo = explode('/',$server_link);
+		$server_link_seo = $server_link_seo[count($server_link_seo) -1 ];
+		
+		//Now finally Checking if both are equal else redirect to new link
+		if($vid_link_seo != $server_link_seo)
+		{
+			//Redirect to valid link leaving mark 301 Permanent Redirect
+			header ('HTTP/1.1 301 Moved Permanently');
+  			header ('Location: '.$vid_link);
+			exit();
+		}
+		
+	}
+	
 	//Checking for playlist
 	$pid = $_GET['play_list'];
 	if(!empty($pid))
