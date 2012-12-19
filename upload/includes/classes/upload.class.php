d431ea212bab808a38854ba41fdc003156f6574b
Arslan Hassan <arslan@clip-bucket.com>
2012-12-19 13:32:25 +0000

    Removed : upload feed (old)

--- upload/includes/classes/upload.class.php
+++ upload/includes/classes/upload.class.php
@@ -214,7 +214,7 @@ class Upload {
         }
 
         //Adding Video Feed
-        addFeed(array('action' => 'upload_video', 'object_id' => $insert_id, 'object' => 'video'));
+        //addFeed(array('action' => 'upload_video', 'object_id' => $insert_id, 'object' => 'video'));
         
         return $insert_id;
     }
