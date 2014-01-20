ba6510da67f5fdbe8aba0496554212b78f65a96f
Arslan Hassan <arslan@clip-bucket.com>
2014-01-20 13:11:53 +0000

    Deleted : Cache

--- upload/cache/%%B6^B66^B66EE130%%pplayer_head.html.php
+++
@@ -1,5 +0,0 @@
-<?php /* Smarty version 2.6.18, created on 2014-01-10 19:00:19
-         compiled from /var/www/clipbucket/player/pak_player/pplayer_head.html */ ?>
-<!--Including Pak Player HTML -->
-<script src="<?php echo @PAK_PLAYER_URL; ?>
-/pplayer.js" type="text/javascript"></script>
