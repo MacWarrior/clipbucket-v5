b3090e3cd9529cc88db660e514966a834daf8d5c
umaircb <umairiu>
2014-01-27 06:57:20 +0000

    admin_area/add_group.php.

--- upload/admin_area/add_group.php
+++ upload/admin_area/add_group.php
@@ -2,5 +2,19 @@
 
 require_once '../includes/admin_config.php';
 $userquery->admin_login_check();
-header("location:".cblink(array("name"=>"create_group")));
+$userquery->login_check('member_moderation');
+$pages->page_redir();
+
+if(isset($_POST['add_group']))
+{
+    if($userquery->signup_user($_POST))
+    {
+        e(lang("new_mem_added"),"m");
+        $_POST = '';
+    }
+}
+
+subtitle("Add New group");
+template_files('add_group.html');
+display_it();
 ?>
