57e5bc8313adba77d04aea0252f07b94e6e0c477
umaircb <umairiu>
2014-03-21 06:40:57 +0000

    breadcrumb added

--- upload/plugins/clipbucket_helper/admin/cron_jobs.php
+++ upload/plugins/clipbucket_helper/admin/cron_jobs.php
@@ -1,4 +1,12 @@
 <?php
 
-template_files('cron_jobs.html',PLUG_DIR.'/clipbucket_helper/admin');
+/* Assigning page and subpage */
+if(!defined('MAIN_PAGE')){
+    define('MAIN_PAGE', 'Clip-Bucket Helper');
+}
+if(!defined('SUB_PAGE')){
+    define('SUB_PAGE', 'Cron Jobs');
+}
+
+template_files(PLUG_DIR.'/clipbucket_helper/admin/cron_jobs.html',true);
 ?>
