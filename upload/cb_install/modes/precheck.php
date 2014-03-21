b97d8be47b18d5bc9b8020fd5310d15c6c65dfdd
sajjadcb <sajjadcb>
2014-03-21 11:22:43 +0000

    fixed: modes checking

--- upload/cb_install/modes/precheck.php
+++ upload/cb_install/modes/precheck.php
@@ -1,5 +1,5 @@
 <?php 
-$php_ver = check_module('php'); var_dump($php_ver);die();
+$php_ver = check_module('php');
 ?>
 <h2>Prechecking</h2>
 Clipbucket requires following modules in order to work properly, we are performing some initial search to find modules.
@@ -11,23 +11,23 @@ Clipbucket requires following modules in order to work properly, we are performi
 <dl>
     <dt>PHP</dt>
     	<?php $php_ver = check_module('php'); var_dump($php_ver)  ?>
-    	<dd><?=msg_arr($php_ver);?></dd>
+    	<dd><?php echo msg_arr($php_ver);?></dd>
     <dt>FFMPEG</dt>
     	<?php $ffmpeg_ver = check_module('ffmpeg'); ?>
-    <dd><?=msg_arr($ffmpeg_ver);?></dd>
+    <dd><?php echo msg_arr($ffmpeg_ver);?></dd>
     <dt>FLVtool2</dt>
          <?php $flvtool2_ver = check_module('flvtool2'); ?>
-    	<dd><?=msg_arr($flvtool2_ver);?></dd>
+    	<dd><?php echo msg_arr($flvtool2_ver);?></dd>
         
     <dt>MP4Box</dt>
     	<?php $mp4boxver = check_module('mp4box'); ?>
-    	<dd><?=msg_arr($mp4boxver);?></dd>
+    	<dd><?php echo msg_arr($mp4boxver);?></dd>
     <dt>cURL</dt>
     	<?php $curlver = check_module('curl'); ?>
-    	<dd><?=msg_arr($curlver);?></dd>
+    	<dd><?php echo msg_arr($curlver);?></dd>
     <dt>PHPShield</dt>
     	<?php $phpshield = check_module('phpshield'); ?>
-    	<dd><?=msg_arr($phpshield);?></dd>
+    	<dd><?php echo msg_arr($phpshield);?></dd>
 </dl>
 <p></p>
 
