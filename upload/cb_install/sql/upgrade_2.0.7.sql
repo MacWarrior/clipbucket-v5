47d5b09461ae068a4b5d89a200643550cfa125af
umaircb <umairiu>
2014-04-16 12:12:15 +0000

    modified.

--- upload/cb_install/sql/upgrade_2.0.7.sql
+++ upload/cb_install/sql/upgrade_2.0.7.sql
@@ -6,3 +6,18 @@ INSERT INTO `{tbl_prefix}config` (`configid`, `name`, `value`) VALUES (NULL, 'lo
 INSERT INTO `{tbl_prefix}config` (`configid`, `name`, `value`) VALUES (NULL, 'buffer_time', '3');
 INSERT INTO `{tbl_prefix}config` (`configid`, `name`, `value`) VALUES (NULL, 'youtube_enabled', 'yes');
 ALTER TABLE `{tbl_prefix}contacts` ADD `request_type` ENUM( 'in', 'out' ) NOT NULL AFTER `contact_group_id` ;
+
+
+
+--
+-- Table structure for table `cb_admin_todo`
+--
+
+CREATE TABLE IF NOT EXISTS `{tbl_prefix}admin_todo` (
+  `todo_id` int(225) NOT NULL AUTO_INCREMENT,
+  `todo` text CHARACTER SET ucs2 NOT NULL,
+  `date_added` datetime NOT NULL,
+  `userid` int(225) NOT NULL,
+  PRIMARY KEY (`todo_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
