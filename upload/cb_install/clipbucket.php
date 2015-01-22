fe916273c48e2ffe099b760298a12478b03f4b14
Fahad Abbas <fahad-cb@users.noreply.github.com>
2015-01-22 07:29:17 +0000

    modified : revision and version changed modified : version '2.7.0.3' added in array

--- upload/cb_install/clipbucket.php
+++ upload/cb_install/clipbucket.php
@@ -10,9 +10,9 @@
  */
  
  define("ClipBucket","ClipBucket - Open Source Media Sharing Script by Arslan Hassan");
- define("VERSION","2.7.0.02");
+ define("VERSION","2.7.0.3");
  define("STATE","Beta");
- define("REV","2902");
+ define("REV","2922");
  define("RELEASED","24-11-2014");
  define("AUTHORS","ARSLAN HASSAN,FAWAZ TAHIR");
 
