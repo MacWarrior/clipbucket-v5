866c14283008f7fb0d164f0a8397a81359fbdab6
Fahad Abbas <fahad-cb@users.noreply.github.com>
2015-01-22 14:20:27 +0000

    modified : revision changed modified : upload thumbs removed

--- upload/cb_install/clipbucket.php
+++ upload/cb_install/clipbucket.php
@@ -12,7 +12,7 @@
  define("ClipBucket","ClipBucket - Open Source Media Sharing Script by Arslan Hassan");
  define("VERSION","2.7.0.3");
  define("STATE","Beta");
- define("REV","2922");
+ define("REV","2926");
  define("RELEASED","24-11-2014");
  define("AUTHORS","ARSLAN HASSAN,FAWAZ TAHIR");
 
--- upload/plugins/cb_link_video/cb_link_video.php
+++ upload/plugins/cb_link_video/cb_link_video.php
@@ -170,37 +170,6 @@
 	}
 	
 	
-	if(!function_exists('upload_thumb'))
-	{
-		function upload_thumb($array)
-		{
-			
-			global $file_name,$LANG;
-			
-			//Get File Name
-			$file 		= $array['name'];
-			$ext 		= getExt($file);
-			$image = new ResizeImage();
-			
-			if(!empty($file) && file_exists($array['tmp_name']) &&!error())
-			{
-				if($image->ValidateImage($array['tmp_name'],$ext)){
-					$file = BASEDIR.'/files/thumbs/'.$_POST['file_name'].'.'.$ext;
-					$bfile = BASEDIR.'/files/thumbs/'.$_POST['file_name'].'.-big.'.$ext;
-					if(!file_exists($file))
-					{
-						move_uploaded_file($array['tmp_name'],$file);
-						$image->CreateThumb($file,$bfile,config('big_thumb_width'),$ext,config('big_thumb_height'),false);
-						$image->CreateThumb($file,$file,THUMB_WIDTH,$ext,THUMB_HEIGHT,false);
-					}
-				}else{
-					e(lang('vdo_thumb_up_err'));
-				}
-			}else{
-				return true;
-			}
-		}
-	}
 	
 	
 	/**
