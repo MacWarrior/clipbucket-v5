cfbfbf4c0d6d4d12e987c06942615ef0c7f16522
Arslan Hassan <arslan@clip-bucket.com>
2015-02-25 15:11:47 +0000

    Updated : Sql statements for sql performance

--- upload/cb_install/sql/structure.sql
+++ upload/cb_install/sql/structure.sql
@@ -1186,5 +1186,21 @@ CREATE TABLE IF NOT EXISTS `{tbl_prefix}admin_todo` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 ALTER TABLE `{tbl_prefix}action_log` DROP `action_link`; 
-ALTER TABLE  `{tbl_prefix}video` ADD  `file_directory` VARCHAR( 10 ) NOT NULL AFTER  `file_server_path`
+ALTER TABLE  `{tbl_prefix}video` ADD  `file_directory` VARCHAR( 10 ) NOT NULL AFTER  `file_server_path`;
+
+-- For Sql performance improvements
+-- @Author : <jozo@jozo.sk>
+
+ALTER TABLE  `{tbl_prefix}plugins` ADD INDEX (  `plugin_active` );
+ALTER TABLE  `{tbl_prefix}sessions` ADD INDEX (  `session` );
+ALTER TABLE  `{tbl_prefix}languages` ADD INDEX (  `language_default` );
+ALTER TABLE  `{tbl_prefix}conversion_queue` ADD INDEX (  `cqueue_conversion` );
+ALTER TABLE  `{tbl_prefix}video` ADD INDEX (  `status`,`active`,`broadcast`,`userid` );
+ALTER TABLE  `{tbl_prefix}user_levels_permissions` ADD INDEX (  `user_level_id` );
+ALTER TABLE  `{tbl_prefix}video` ADD INDEX (  `userid` );
+ALTER TABLE  `{tbl_prefix}users` ADD INDEX (  `username`(255),`userid` );
+ALTER TABLE  `{tbl_prefix}pages` ADD INDEX (  `active`,`display` );
+ALTER TABLE  `{tbl_prefix}video` ADD INDEX (  `videoid`,`videokey`(255) );
+ALTER TABLE  `{tbl_prefix}languages` ADD INDEX (  `language_code`,`language_id` );
+ALTER TABLE  `{tbl_prefix}video_categories` ADD INDEX (  `parent_id` );
 
