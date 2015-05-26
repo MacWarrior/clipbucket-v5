6c16304e7ebaeeb718a876a6dd40c50fdd2e6daf
Saqib Razzaq <saqirzzq@users.noreply.github.com>
2015-05-26 14:06:16 +0000

    Extra signup link in header removed

--- upload/includes/classes/ClipBucket.class.php
+++ upload/includes/classes/ClipBucket.class.php
@@ -515,10 +515,6 @@ class ClipBucket
         $this->head_menu[] = array('name' => lang("menu_channels"),'icon' => '<i class="fa fa-desktop"></i>', 'link' => cblink(array('name' => 'channels')), "this" => "channels", "section" => "channels");
         $this->head_menu[] = array('name' => lang("collections"), 'icon' => '<i class="fa fa-bars"></i>', 'link' => cblink(array('name' => 'collections')), "this" => "collections", "section" => "collections");
         $this->head_menu[] = array('name' => lang("Groups"), 'icon' => '<i class="fa fa-users"></i>', 'link' => cblink(array('name' => 'groups')), "this" => "groups", "section" => "groups");
-        if (!userid())
-            $this->head_menu[] = array('name' => lang("signup"), 'icon' => '<i class="fa fa-pencil-square-o"></i>', 'link' => cblink(array('name' => 'signup')), "this" => "signup");
-
-        
 
         /* Calling custom functions for headMenu. This can be used to add new tabs */
         //cb_call_functions('headMenu');
