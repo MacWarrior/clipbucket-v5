2eee3385220e768d5c5bfb92ebfab80e255fba94
Fahad Abbas <fahad-cb@users.noreply.github.com>
2015-05-28 09:53:14 +0000

    updated: issue resolved (Some channels don't show up only in search)

--- upload/includes/classes/user.class.php
+++ upload/includes/classes/user.class.php
@@ -4088,8 +4088,12 @@ class userquery extends CBCategory{
 	{
 		$this->search = new cbsearch;
 		$this->search->db_tbl = "users";
+		// added more conditions usr_status='Ok' and ban_status='no'
 		$this->search->columns =array(
 			array('field'=>'username','type'=>'LIKE','var'=>'%{KEY}%'),
+			array('field'=>'usr_status','type'=>'=','var'=>'Ok','op'=>'AND','value'=>'static'),
+			array('field'=>'ban_status','type'=>'=','var'=>'no','op'=>'AND','value'=>'static'),
+			
 		);
 		$this->search->cat_tbl = $this->cat_tbl;
 
