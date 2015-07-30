b0a226e1dcc874c07cf4520edb5a479c28063daf
Fahad Abbas <fahad-cb@users.noreply.github.com>
2015-07-30 16:13:20 +0000

    modified : cb 2.7.6 release pre release

--- upload/cb_install/clipbucket.php
+++ upload/cb_install/clipbucket.php
@@ -12,7 +12,7 @@
  define("ClipBucket","ClipBucket - Open Source Media Sharing Script by Arslan Hassan");
  define("VERSION","2.7.6");
  define("STATE","STABLE");
- define("REV","3248");
+ define("REV","3249");
  define("RELEASED","30-07-2015");
  define("AUTHORS","ARSLAN HASSAN,FAWAZ TAHIR");
 
--- upload/cb_install/sql/upgrade_2.7.6.sql
+++ upload/cb_install/sql/upgrade_2.7.6.sql
@@ -1,3 +1,4 @@
 -- Addition for 2.7.6
 UPDATE `{tbl_prefix}config` SET value = 'cb_video_js.php' WHERE name = 'player_file';
+UPDATE `{tbl_prefix}config` SET value = 'cb_video_js' WHERE name = 'player_dir';
 
