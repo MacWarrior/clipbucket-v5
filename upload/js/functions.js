eb78a2e06d2b81bec0729db5f066396bba3b1657
Saqib Razzaq <saqirzzq@users.noreply.github.com>
2015-08-10 09:10:03 +0000

    Remote Upload fixed

--- upload/js/functions.js
+++ upload/js/functions.js
@@ -229,10 +229,11 @@ var loading_img_2 = "<img style='vertical-align:middle' src='"+imageurl+"/ajax-l
 			  },
 			  success: function(data)
 			  {
-				 if ( youtube_mania_installed == true ) 
-				  	{
-				  		extract_yt_url_ajax(data);
-				  	}  
+			  	if (!(typeof(extract_yt_url_ajax)=='undefined')) 
+			  		{
+			  			extract_yt_url_ajax(data);
+			  		}
+
 				  if(data.error)
 				  {		  
 					force_stop = true;
