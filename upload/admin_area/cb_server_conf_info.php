980ea2db8a7e5d71b7b530fc6c7cc7aa966b9ed0
Fahad Abbas <fahad-cb@users.noreply.github.com>
2015-08-17 11:22:51 +0000

    cb_server_conf added

---
+++ upload/admin_area/cb_server_conf_info.php
@@ -0,0 +1,36 @@
+<?php
+/* 
+ ***************************************************************
+ | Copyright (c) 2007-2008 Clip-Bucket.com. All rights reserved.
+ | @ Author : ArslanHassan										
+ | @ Software : ClipBucket , © PHPBucket.com					
+ ****************************************************************
+*/
+require'../includes/admin_config.php';
+$userquery->admin_login_check();
+$pages->page_redir();
+
+/* Assigning page and subpage */
+if(!defined('MAIN_PAGE')){
+	define('MAIN_PAGE', 'Tool Box');
+}
+if(!defined('SUB_PAGE')){
+	define('SUB_PAGE', 'PHP extensions info');
+}
+
+
+$post_max_size = ini_get('post_max_size');
+$memory_limit = ini_get('memory_limit');
+$upload_max_filesize = ini_get('upload_max_filesize');
+$max_execution_time = ini_get('max_execution_time');
+
+assign("post_max_size",$post_max_size);
+assign("memory_limit",$memory_limit);
+assign("upload_max_filesize",$upload_max_filesize);
+assign("max_execution_time",$max_execution_time);
+ 
+
+subtitle("ClipBucket Server Module Checker");
+template_files("cb_server_conf_info.html");
+display_it();
+?>
