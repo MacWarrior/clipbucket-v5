47183963e251fb47f5c7fac5e211143a802959ee
Saqib Razzaq <saqirzzq@users.noreply.github.com>
2015-10-21 07:24:15 +0000

    ClipBucket version displayed

--- upload/admin_area/cb_server_conf_info.php
+++ upload/admin_area/cb_server_conf_info.php
@@ -6,6 +6,7 @@
  | @ Software : ClipBucket , © PHPBucket.com					
  ****************************************************************
 */
+
 require'../includes/admin_config.php';
 $userquery->admin_login_check();
 $pages->page_redir();
@@ -18,7 +19,6 @@ if(!defined('SUB_PAGE')){
 	define('SUB_PAGE', 'PHP extensions info');
 }
 
-
 $post_max_size = ini_get('post_max_size');
 $memory_limit = ini_get('memory_limit');
 $upload_max_filesize = ini_get('upload_max_filesize');
@@ -28,7 +28,7 @@ assign("post_max_size",$post_max_size);
 assign("memory_limit",$memory_limit);
 assign("upload_max_filesize",$upload_max_filesize);
 assign("max_execution_time",$max_execution_time);
- 
+assign('VERSION',VERSION);
 
 subtitle("ClipBucket Server Module Checker");
 template_files("cb_server_conf_info.html");
