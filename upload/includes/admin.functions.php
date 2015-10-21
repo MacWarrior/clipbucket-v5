6588fd279226bd0a4388baf1b0e943f40002921a
Saqib Razzaq <saqirzzq@users.noreply.github.com>
2015-10-21 07:06:56 +0000

    Function defined in different file now

--- upload/includes/admin.functions.php
+++ upload/includes/admin.functions.php
@@ -21,28 +21,6 @@ if(has_access('admin_access',TRUE))
 		echo '<a href="'.ADMIN_BASEURL.'/view_user.php?uid='.$data['userid'].'">Edit User</a>';
 	}
 
-	function check_server_confs() 
-	{
-		define('POST_MAX_SIZE', ini_get('post_max_size'));
-	    define('MEMORY_LIMIT', ini_get('memory_limit'));
-	    define('UPLOAD_MAX_FILESIZE', ini_get('upload_max_filesize'));
-	    define('MAX_EXECUTION_TIME', ini_get('max_execution_time'));
-
-		if ( POST_MAX_SIZE > 50 && MEMORY_LIMIT > 128 && UPLOAD_MAX_FILESIZE > 50 && MAX_EXECUTION_TIME > 7200 ) 
-		{
-			define("SERVER_CONFS", true);
-		} 
-		elseif ( POST_MAX_SIZE < 50 || MEMORY_LIMIT < 128 || UPLOAD_MAX_FILESIZE || 50 && MAX_EXECUTION_TIME || 7200 )
-		{
-			e('You must update <strong>"Server Configurations"</strong>. Click here <a href='.BASEURL.'/admin_area/cb_server_conf_info.php>for details</a>',w);
-			define("SERVER_CONFS", false);
-		}
-		else 
-		{
-			define("SERVER_CONFS", false);
-		}
-	}
-
 	register_anchor_function('show_video_admin_link','watch_admin_options');
 	register_anchor_function('show_view_channel_link','view_channel_admin_options');
 }
--- upload/includes/functions.php
+++ upload/includes/functions.php
@@ -5270,6 +5270,34 @@
 		}
 	}
 
+	/*
+	* Function used to check server configs
+	* Checks : MEMORY_LIMIT, UPLOAD_MAX_FILESIZE, POST_MAX_SIZE, MAX_EXECUTION_TIME
+	* If any of these configs are less than required value, warning is shown
+    */
+
+	function check_server_confs() 
+	{
+		define('POST_MAX_SIZE', ini_get('post_max_size'));
+	    define('MEMORY_LIMIT', ini_get('memory_limit'));
+	    define('UPLOAD_MAX_FILESIZE', ini_get('upload_max_filesize'));
+	    define('MAX_EXECUTION_TIME', ini_get('max_execution_time'));
+
+		if ( POST_MAX_SIZE > 50 && MEMORY_LIMIT > 128 && UPLOAD_MAX_FILESIZE > 50 && MAX_EXECUTION_TIME > 7200 ) 
+		{
+			define("SERVER_CONFS", true);
+		} 
+		elseif ( POST_MAX_SIZE < 50 || MEMORY_LIMIT < 128 || UPLOAD_MAX_FILESIZE || 50 && MAX_EXECUTION_TIME || 7200 )
+		{
+			e('You must update <strong>"Server Configurations"</strong>. Click here <a href='.BASEURL.'/admin_area/cb_server_conf_info.php>for details</a>',w);
+			define("SERVER_CONFS", false);
+		}
+		else 
+		{
+			define("SERVER_CONFS", false);
+		}
+	}
+
         include( 'functions_db.php' );
         include( 'functions_filter.php' );
         include( 'functions_player.php' );
