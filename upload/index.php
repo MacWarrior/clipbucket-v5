d44413960a740ceb3d8c3c2848e8438c81523326
Saqib Razzaq <saqirzzq@users.noreply.github.com>
2016-02-26 07:09:29 +0000

    Code documentation updated

--- upload/index.php
+++ upload/index.php
@@ -1,24 +1,22 @@
 <?php
-//test comment
-/* 
- ***************************************************************
- | Copyright (c) 2007-2010 Clip-Bucket.com. All rights reserved.
- | @ Author	   : ArslanHassan									
- | @ Software  : ClipBucket , � PHPBucket.com					
- ****************************************************************
+
+/**
+* File: Index
+* Description: FIle used for dispaying home page
+* @author: Arslan Hassan
+* @since: 2007
+* @website: clip-bucket.com
+* Copyright (c) 2007-2016 Clip-Bucket.com. All rights reserved
 */
+
 define('THIS_PAGE','index');
-
 require 'includes/config.inc.php';
-
-
 $pages->page_redir();
 
 if(is_installed('editorspick'))
 {
 	assign('editor_picks',get_ep_videos());
 }
-
 //Displaying The Template
 template_files('index.html');
 display_it();
