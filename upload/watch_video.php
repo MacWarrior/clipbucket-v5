30f7a406eaab3f3356aaa0726ff7b0867597e5c3
Saqib Razzaq <saqirzzq@users.noreply.github.com>
2016-02-26 08:32:16 +0000

    Watch video optimized

--- upload/watch_video.php
+++ upload/watch_video.php
@@ -1,105 +1,21 @@
 <?php
-/* 
- ******************************************************************
- | Copyright (c) 2007-2009 Clip-Bucket.com. All rights reserved.	
- | @ Author : ArslanHassan											
- | @ Software : ClipBucket , © PHPBucket.com						
- *******************************************************************
-*/
 
-define("THIS_PAGE",'watch_video');
-define("PARENT_PAGE",'videos');
-require 'includes/config.inc.php';
-$userquery->perm_check('view_video',true);
-$pages->page_redir();
-
-//Getting Video Key
-$vkey = @$_GET['v'];
-$vkey = mysql_clean($vkey);
-$vdo = $cbvid->get_video($vkey);
-
-//pr($vdo,true);
-
-assign('vdo',$vdo);
-if(video_playable($vdo))
-{	
-	
 	/**
-	 * Please check http://code.google.com/p/clipbucket/issues/detail?id=168
-	 * for more details about following code
-	 */
-	 
-	if(SEO=='yes')
-	{
-		//Checking if Video URL is Exactly What we have created
-		$vid_link = videoLink($vdo);
-		$vid_link_seo = explode('/',$vid_link);
-		$vid_link_seo = $vid_link_seo[count($vid_link_seo) -1 ];
-		
-		//What we are getting
-		$server_link = $_SERVER['REQUEST_URI'];
-		$server_link_seo = explode('/',$server_link);
-		$server_link_seo = $server_link_seo[count($server_link_seo) -1 ];
-		
-		//Now finally Checking if both are equal else redirect to new link
-		if($vid_link_seo != $server_link_seo)
-		{
-			/*//Redirect to valid link leaving mark 301 Permanent Redirect
-			header ('HTTP/1.1 301 Moved Permanently');
-  			header ('Location: '.$vid_link);
-			exit();*/
-		}
-		
-	}
-	
-	//Checking for playlist
+	* File: watch_video
+	* Description: FIle used to display watch video page
+	* @author: Arslan Hassan, Saqib Razzaq
+	* @since: 2007
+	* @website: clip-bucket.com
+	* @modified: Feb 26, 2016 ClipBucket 2.8.1 [ Saqib Razzaq ]
+	*/
 
-	$pid = $_GET['play_list'];
-	if(!empty($pid))
-	{
-		$plist = get_playlist( $pid );
-		if ( $plist ) {
-            assign( 'playlist', $plist );
-        }
-	}
-	//Calling Functions When Video Is going to play
-	call_watch_video_function($vdo);
-	
-	subtitle($vdo['title']);
-	
-}else
-	$Cbucket->show_page = false;
-
-//Return category id without '#'
-$v_cat = $vdo['category'];
-if($v_cat[2] =='#') {
-$video_cat = $v_cat[1];
-}else{
-$video_cat = $v_cat[1].$v_cat[2];}
-$vid_cat = str_replace('%#%','',$video_cat);
-assign('vid_cat',$vid_cat);
-
-
-$title = $vdo['title'];
-$tags = $vdo['tags'];
-$videoid = $vdo['videoid'];
-
-$related_videos = get_videos(array('title'=>$title,'tags'=>$tags,
-'exclude'=>$videoid,'show_related'=>'yes','limit'=>12,'order'=>'date_added DESC'));
-if(!$related_videos){
-	$related_videos  = get_videos(array('exclude'=>$videoid,'limit'=>12,'order'=>'date_added DESC'));
-}
-$playlist = $cbvid->action->get_playlist($pid,userid());
-assign('playlist',$playlist);
-			//Getting Playlist Item
-			$items = $cbvid->get_playlist_items( $pid, 'playlist_items.date_added DESC' );
-			assign('items',$items);
-
-Assign('videos', $related_videos);
-      
-
-
-//Displaying The Template
-template_files('watch_video.html');
-display_it();
+	define("THIS_PAGE",'watch_video');
+	define("PARENT_PAGE",'videos');
+	require 'includes/config.inc.php';
+	global $hlp;
+	$assign_arry = array();
+	$userquery->perm_check('view_video',true);
+	$pages->page_redir();
+	$hlp->watch_video();
+	display_it();
 ?>
