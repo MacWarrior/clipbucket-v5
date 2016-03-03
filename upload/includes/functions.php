a04afdf2cb61ca26aa6bd719cb654b90f431e9ae
Saqib Razzaq <saqib@clipbucket.com>
2016-03-02 23:08:39 -0800

    - action : modified get_browser_details() function - description : documentation added

--- upload/includes/functions.php
+++ upload/includes/functions.php
@@ -4781,15 +4781,18 @@
 
 	
 	/**
-	 * function used to get user agent details
-	 * Thanks to ruudrp at live dot nl 28-Nov-2010 11:31 PHP.NET
-	 */
-	function get_browser_details($in=NULL,$assign=false)
-	{
+	* Fetch browser details for current user
+	* 
+	* @param : { string } { $in } { false by default, HTTP_USER_AGENT } 
+	* @param : { boolean } { $assign } { false by default, assigns browser details to this if true }
+	* @return : { array } { $array } { array with all details of user }
+	*/
+
+	function get_browser_details($in=NULL,$assign=false) {
 		//Checking if browser is firefox
-		if(!$in)
+		if(!$in) {
 			$in = $_SERVER['HTTP_USER_AGENT'];
-		
+		}
 		$u_agent = $in;
 		$bname = 'Unknown';
 		$platform = 'Unknown';
@@ -4798,66 +4801,46 @@
 		//First get the platform?
 		if (preg_match('/linux/i', $u_agent)) {
 			$platform = 'linux';
-		}
-		elseif (preg_match('/iPhone/i', $u_agent)) {
+		} elseif (preg_match('/iPhone/i', $u_agent)) {
 			$platform = 'iphone';
-		}
-		elseif (preg_match('/iPad/i', $u_agent)) {
+		} elseif (preg_match('/iPad/i', $u_agent)) {
 			$platform = 'ipad';
-		}
-		elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
+		} elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
 			$platform = 'mac';
-		}
-		elseif (preg_match('/windows|win32/i', $u_agent)) {
+		} elseif (preg_match('/windows|win32/i', $u_agent)) {
 			$platform = 'windows';
 		}
 	   
 		// Next get the name of the useragent yes seperately and for good reason
-		if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent))
-		{
+		if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent)) {
 			$bname = 'Internet Explorer';
 			$ub = "MSIE";
-		}
-		elseif(preg_match('/Firefox/i',$u_agent))
-		{
+		} elseif(preg_match('/Firefox/i',$u_agent)) {
 			$bname = 'Mozilla Firefox';
 			$ub = "Firefox";
-		}
-		elseif(preg_match('/Chrome/i',$u_agent))
-		{
+		} elseif(preg_match('/Chrome/i',$u_agent)) {
 			$bname = 'Google Chrome';
 			$ub = "Chrome";
-		}
-		elseif(preg_match('/Safari/i',$u_agent))
-		{
+		} elseif(preg_match('/Safari/i',$u_agent)) {
 			$bname = 'Apple Safari';
 			$ub = "Safari";
-		}
-		elseif(preg_match('/Opera/i',$u_agent))
-		{
+		} elseif(preg_match('/Opera/i',$u_agent)) {
 			$bname = 'Opera';
 			$ub = "Opera";
-		}
-		elseif(preg_match('/Netscape/i',$u_agent))
-		{
+		} elseif(preg_match('/Netscape/i',$u_agent)) {
 			$bname = 'Netscape';
 			$ub = "Netscape";
-		}
-		elseif(preg_match('/Googlebot/i',$u_agent))
-		{
+		} elseif(preg_match('/Googlebot/i',$u_agent)) {
 			$bname = 'Googlebot';
 			$ub = "bot";
-		}elseif(preg_match('/msnbot/i',$u_agent))
-		{
+		} elseif(preg_match('/msnbot/i',$u_agent)) {
 			$bname = 'MSNBot';
 			$ub = "bot";
-		}elseif(preg_match('/Yahoo\! Slurp/i',$u_agent))
-		{
+		} elseif(preg_match('/Yahoo\! Slurp/i',$u_agent)) {
 			$bname = 'Yahoo Slurp';
 			$ub = "bot";
 		}
 
-	   
 		// finally get the correct version number
 		$known = array('Version', $ub, 'other');
 		$pattern = '#(?<browser>' . join('|', $known) .
@@ -4873,12 +4856,10 @@
 			//see if version is before or after the name
 			if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
 				$version= $matches['version'][0];
-			}
-			else {
+			} else {
 				$version= $matches['version'][1];
 			}
-		}
-		else {
+		} else {
 			$version= $matches['version'][0];
 		}
 	   
@@ -4896,9 +4877,8 @@
 		
 		if($assign)	assign($assign,$array); else return $array;
 	}
-	
-	function update_user_voted($array,$userid=NULL)
-	{
+
+	function update_user_voted($array,$userid=NULL) {
 		global $userquery;
 		return $userquery->update_user_voted($array,$userid);	
 	}
