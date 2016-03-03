eb3d7cf53a6005177eb0af410e59a6169064586a
Saqib Razzaq <saqib@clipbucket.com>
2016-03-03 02:38:27 -0800

    documentation added for is_ssl() function

--- upload/includes/functions.php
+++ upload/includes/functions.php
@@ -4296,18 +4296,21 @@
 	
 	
 	/**
-	 * Determine if SSL is used.
-	 *
-	 * @since 2.6.0
-	 *
-	 * @return bool True if SSL, false if not used.
-	 */
+	* Check if website is using SSL or not
+	*
+	* @param { none }
+	* @return { boolean } { true if SSL, else fasle }
+	* @since 2.6.0
+	*/
+
 	function is_ssl() {
-		if ( isset($_SERVER['HTTPS']) ) {
-			if ( 'on' == strtolower($_SERVER['HTTPS']) )
+		if (isset($_SERVER['HTTPS'])) {
+			if ('on' == strtolower($_SERVER['HTTPS'])) {
 				return true;
-			if ( '1' == $_SERVER['HTTPS'] )
+			}
+			if ('1' == $_SERVER['HTTPS']) {
 				return true;
+			}
 		} elseif ( isset($_SERVER['SERVER_PORT']) && ( '443' == $_SERVER['SERVER_PORT'] ) ) {
 			return true;
 		}
