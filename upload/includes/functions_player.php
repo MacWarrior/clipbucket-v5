6be7d33c00739913abb69462c1052f940710706d
Saqib Razzaq <saqib@clipbucket.com>
2016-03-15 22:40:48 -0700

    Documentation + code structure modified

--- upload/includes/functions_player.php
+++ upload/includes/functions_player.php
@@ -1,51 +1,44 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
- * User: Fawaz
- * Date: 8/28/13
- * Time: 10:08 AM
+ * All functions regarding ClipBucket video players are here
+ * @author: Fawaz Tahir
+ * @since: 28th August, 2013
  * To change this template use File | Settings | File Templates.
  */
 
 /**
- * Function used to display flash player for ClipBucket video
- */
-function flashPlayer($param)
-{
-    global $Cbucket,$swfobj;
-    $param['player_div'] = $param['player_div'] ? $param['player_div'] : 'videoPlayer';
+* Function used to display flash player for ClipBucket video
+* 
+* @param : { array } { $param } { an array of parameters }
+*/
 
-    $key 		= $param['key'];
-    $flv 		= $param['flv'].'.flv';
-    $code 		= $param['code'];
-    $flv_url 	= $file;
-    $embed 		= $param['embed'];
-    $code 		= $param['code'];
-    $height 	= $param['height'] ? $param['height'] : config('player_height');
-    $width 		= $param['width'] ? $param['width'] : config('player_width');
+function flashPlayer($param) {
+    global $Cbucket,$swfobj;
+    global $pak_player;
+    $param['player_div'] = $param['player_div'] ? $param['player_div'] : 'videoPlayer';
+    $key        = $param['key'];
+    $flv        = $param['flv'].'.flv';
+    $code       = $param['code'];
+    $flv_url    = $file;
+    $embed      = $param['embed'];
+    $code       = $param['code'];
+    $height     = $param['height'] ? $param['height'] : config('player_height');
+    $width      = $param['width'] ? $param['width'] : config('player_width');
     $param['height'] = $height;
     $param['width'] = $width ;
     $param['enlarge_button'] = config('enlarge_button');
     $param['player_logo_url'] = config('player_logo_url');
     //dump($code);
-
-    if(!$param['autoplay'])
+    if(!$param['autoplay']) {
         $param['autoplay'] = config('autoplay_video');
-
+    }
     assign('player_params',$param);
-
-
-
-    if(count($Cbucket->actions_play_video)>0)
-    {
-        foreach($Cbucket->actions_play_video as $funcs )
-        {
-            if(function_exists($funcs))
-            {
+    if(count($Cbucket->actions_play_video)>0) {
+        foreach($Cbucket->actions_play_video as $funcs) {
+            if(function_exists($funcs)) {
                 $func_data = $funcs($param);
             }
-            if($func_data)
-            {
+            if($func_data) {
                 $player_code = $func_data;
                 $show_player  = true;
                 break;
@@ -53,26 +46,22 @@ function flashPlayer($param)
         }
     }
 
-    if(function_exists('cbplayer') && empty($player_code))
+    if(function_exists('cbplayer') && empty($player_code)) {
         $player_code = cbplayer($param,true);
-    elseif(function_exists('cbplayer'))
+    } elseif(function_exists('cbplayer')) {
         return $player_code;
+    }
 
-
-
-    global $pak_player;
-
-    if($player_code)
-        if(!$pak_player && $show_player && !is_bool($player_code))
-        {
+    if($player_code) {
+        if(!$pak_player && $show_player && !is_bool($player_code)) {
             assign("player_js_code",$player_code);
             Template(PLAYER_DIR.'/player.html',false);
             return false;
-        }else
-        {
+        } else {
 
             return false;
         }
+    }
 
     return blank_screen($param);
 }
