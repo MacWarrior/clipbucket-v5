20a544475ab5aedccb2634eed9994ae9e64a7b4e
Saqib Razzaq <saqi.cb@gmail.com>
2016-03-21 11:25:28 +0500

    Documentation added + file cleaned

--- upload/index.php
+++ upload/index.php
@@ -1,24 +1,24 @@
 <?php
 
-/**
-* File: Index
-* Description: FIle used for dispaying home page
-* @author: Arslan Hassan
-* @since: 2007
-* @website: clip-bucket.com
-* Copyright (c) 2007-2016 Clip-Bucket.com. All rights reserved
-*/
+	/**
+	* File: Index
+	* Description: This is home page of your wesbite. Anyone who lands on your website
+	* using home page URL will see this page first
+	* @author: Arslan Hassan
+	* @since: 2007, ClipBucket v2.0
+	* @website: clip-bucket.com
+	* Copyright (c) 2007-2016 Clip-Bucket.com. All rights reserved
+	*/
 
-define('THIS_PAGE','index');
-require 'includes/config.inc.php';
-$pages->page_redir();
+	define('THIS_PAGE','index');
+	require 'includes/config.inc.php';
+	$pages->page_redir();
+	if(is_installed('editorspick')) {
+		assign('editor_picks',get_ep_videos());
+	}
 
-if(is_installed('editorspick'))
-{
-	assign('editor_picks',get_ep_videos());
-}
-//Displaying The Template
-template_files('index.html');
-display_it();
+	//Displaying The Template
+	template_files('index.html');
+	display_it();
 
 ?>
