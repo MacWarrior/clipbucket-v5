3f43443d8be92dd6033170ce43b6bc70113989cb
Saqib Razzaq <saqi.cb@gmail.com>
2016-03-25 15:33:47 +0500

    Admin area action logs files added

---
+++ upload/admin_area/action_logs.php
@@ -0,0 +1,43 @@
+<?php
+/* 
+ ***********************************************************************
+ | Copyright (c) 2007-2010 Clip-Bucket.com. All rights reserved.		
+ | @ Author 	: ArslanHassan												
+ | @ Software 	: ClipBucket , © PHPBucket.com							
+ *************************************************************************
+*/
+
+require'../includes/admin_config.php';
+$userquery->admin_login_check();
+$pages->page_redir();
+
+/* Assigning page and subpage */
+if(!defined('MAIN_PAGE')){
+	define('MAIN_PAGE', 'Tool Box');
+}
+if(!defined('SUB_PAGE')){
+	define('SUB_PAGE', 'Action Logs');
+}
+
+//Getting User List
+if (isset($_GET['type'])) {
+	$type = $_GET['type'];
+	$result_array['type'] = $type;
+}
+$result_array['limit'] = 20;
+if(!$array['order'])
+    $result_array['order'] = " DESC ";
+
+
+
+$logs = fetch_action_logs($result_array);
+assign('total_logs',count($logs));
+assign('logs', $logs);
+subtitle("Action Logs");
+template_files('action_logs.html');
+display_it();
+
+
+
+
+?>
