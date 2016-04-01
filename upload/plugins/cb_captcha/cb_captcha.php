cb4ac58e2b62a1352cc4d71d38c7f8dd74108d5f
Saqib Razzaq <saqi.cb@gmail.com>
2016-04-01 14:26:41 +0500

    COde comments + identation

--- upload/plugins/cb_captcha/cb_captcha.php
+++ upload/plugins/cb_captcha/cb_captcha.php
@@ -2,36 +2,30 @@
 /**
 *	Plugin Name: CB Captcha
 *	Description: Stop spam signups like a boss with latest Captcha
-*	ClipBucket version: 2.7.x
-*	Plugin Version: 1.0
-*	@author Saqib Razzaq
+*	ClipBucket: version: 2.7.x
+*	Plugin: Version: 1.0
+*	Author: Saqib Razzaq
 *	@since 9th July, 2015 (ClipBucket 2.7.4)
+* 	@modified: 1st April, 2016 ClipBucket 2.8.1
 */
 	define('CB_CAPTCHA',basename(dirname(__FILE__)));
 
 	// checks user's input
-	function validate_user_ans()
-	{
-		if (isset($_POST['signup']))
-		{
+	function validate_user_ans() {
+		if (isset($_POST['signup'])) {
 			$result = $_POST['g-recaptcha-response'];
-
-			if ($result == '')
-			{
+			if ($result == '') {
 				header("Location: ".BASEURL."/signup.php?valid=fail");
 			}
 		}
 	}
 
-
-	if (isset($_POST['signup']))
-	{
+	if (isset($_POST['signup'])) {
 		validate_user_ans();
 	}
 
 	// displaying the form
-	function the_form()
-	{
+	function the_form() {
 		global $db;
 		$key_check = $db->_select('SELECT the_key FROM '.tbl("the_captcha"));
 		$the_key = $key_check[0]['the_key'];
@@ -40,9 +34,7 @@
 	}
 
 	register_anchor_function("the_form", "the_form");
-
 	add_admin_menu("CB Captcha" , "reCaptcha Key" , "cb_captcha_admin.php" , CB_CAPTCHA);
-
 	// used for displaying message on failure of captcha
 	template_files(PLUG_DIR."/cb_captcha/captcha.html");
 ?>
