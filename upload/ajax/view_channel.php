8922cd9f97614b2bbe57bd42083609e530d25227
Saqib Razzaq <saqi.cb@gmail.com>
2016-04-08 17:52:32 +0500

    Load more added for photos on view channel

--- upload/ajax/view_channel.php
+++ upload/ajax/view_channel.php
@@ -30,6 +30,24 @@
 					echo trim(Fetch('/blocks//videos/video.html'));
 				}
 				break;
+			case 'channelMorePhotos':
+				global $db, $cbvid, $Smarty;
+				$load_hit = $_POST['loadHit'];
+				$load_limit = $_POST['loadLimit'];
+				$user = $_POST['user'];
+				$start = $load_limit * $load_hit - $load_limit;
+				$sql_limit = "$start, $load_limit";
+				$total_items = $_POST['totalPhotos'];
+				$items = get_photos(array("user"=>$user, "order"=>"date_added DESC","limit"=>$sql_limit));
+				if ($start >= $total_items) {
+					return false;
+				}
+				foreach ($items as $key => $p_list) {
+					assign("photo", $p_list);
+					assign("display_type","view_channelAjax");
+					echo trim(Fetch('/blocks/photo.html'));
+				}
+				break;
 			
 			default:
 				# code...
