6409ee54d4a9e4ec855e82aa766eb497e7044acd
Saqib Razzaq <saqi.cb@gmail.com>
2016-04-14 16:32:43 +0500

    Modified remote uploading structure

--- upload/actions/file_downloader.php
+++ upload/actions/file_downloader.php
@@ -82,51 +82,28 @@ if(isset($_POST['youtube']))
 
 	// grabs video details (snippet, contentDetails)
 	$youtube_content = file_get_contents('https://www.googleapis.com/youtube/v3/videos?id='.$YouTubeId.'&key=AIzaSyDOkg-u9jnhP-WnzX5WPJyV1sc5QQrtuyc&part=snippet,contentDetails');
-	$content = json_decode($youtube_content);
+	$content = json_decode($youtube_content,true);
+	$thumb_contents = maxres_youtube($content);
+	$max_quality_thumb = $thumb_contents['thumb'];
 
-	//$content = xml2array('http://gdata.youtube.com/feeds/api/videos/'.$YouTubeId);
-
-	// getting things such as title out of Snippet
-	$data = $content->items[0]->snippet;
+	$data = $content['items'][0]['snippet'];
 
 	// getting time out of contentDetails
-	$time = $content->items[0]->contentDetails->duration;
+	$time = $content['items'][0]['contentDetails']['duration'];
 
 	/**
 	* Converting YouTube Time in seconds
 	*/
 
 	$total = yt_time_convert($time);
-
-	/*	$match_arr = 
-	array
-	(
-		"title"=>"/<meta name=\"title\" content=\"(.*)\">/",
-		"description"=>"/<meta name=\"description\" content=\"(.*)\">/",
-		"tags" =>"/<meta name=\"keywords\" content=\"(.*)\">/",
-		"embed_code" => "/<meta name=\"keywords\" content=\"(.*)\">/",
-		"duration" => "/<span class=\"video-time\">([0-9\:]+)<\/span>/"
-	);
-	
-	$vid_array = array();
-	foreach($match_arr as $title=> $match)
-	{
-		preg_match($match,$content,$matches);
-		$vid_array[$title] = $matches[1];
-	}*/
-	
-	$vid_array['title'] 		= $data->title;
-	$vid_array['description'] 	= $data->description;
-	$vid_array['tags'] 			= $data->title;
+	$vid_array['title'] 		= $data['title'];
+	$vid_array['description'] 	= $data['description'];
+	$vid_array['tags'] 			= $data['title'];
 	$vid_array['duration'] 		= $total;
 	
-	$thumbs_array = $data->thumbnails;
-
-	$vid_array['thumbs'] = 
-	array($thumbs_array->high->url, $thumbs_array->medium->url);
+	$vid_array['thumbs'] = $max_quality_thumb;
 
 	#pex($vid_array['thumbs'],true);
-
 
 	$vid_array['embed_code'] = '<iframe width="560" height="315"';
 	$vid_array['embed_code'] .= ' src="//www.youtube.com/embed/'.$YouTubeId.'" ';
@@ -138,13 +115,6 @@ if(isset($_POST['youtube']))
 	$vid_array['userid'] = userid();
 	
 	$duration = $vid_array['duration'];
-	/*	$duration = explode(":",$duration);
-		$sep = count($duration);
-		if($sep==3)
-			$duration = ($duration[0]*60*60)+($duration[1]*60)+($duration[2]);
-		else
-			$duration = ($duration[0]*60)+($duration[1]);
-	*/
 	$vid = $Upload->submit_upload($vid_array);
 	
 	if(error())
@@ -160,17 +130,19 @@ if(isset($_POST['youtube']))
 	$ref_url = get_refer_url_from_embed_code(unhtmlentities(stripslashes($vdetails['embed_code'])));
 	$ref_url = $ref_url['url'];
 	$db->update(tbl("video"),array("status","refer_url","duration"),array('Successful',$ref_url,$duration)," videoid='$vid'");
-	
+
 	//Downloading thumb
-	foreach($vid_array['thumbs'] as $tid => $thumb)
-	{
-		if($tid!='big')
-			$thumbId = $tid+1;
-		else
-			$thumbId = 'big';
-		snatch_it(urlencode($thumb),THUMBS_DIR.'/'.$file_directory,$filename."-$thumbId.jpg");
-	}
-	
+	$downloaded_thumb = snatch_it(urlencode($max_quality_thumb),THUMBS_DIR.'/'.$file_directory,$filename."-ytmax.jpg");
+
+	$params = array();
+	$params['filepath'] = $downloaded_thumb;
+	$params['files_dir'] = $file_directory;
+	$params['file_name'] = $filename;
+	$params['width'] = $thumb_contents['width'];
+	$params['height'] = $thumb_contents['height'];
+
+	thumbs_black_magic($params);
+
 	exit(json_encode(array('youtubeID'=>$YouTubeId,
 	'vid'=>$vid,
 	'title'=>$vid_array['title'],'desc'=>$vid_array['description'],
