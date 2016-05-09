2a252e3799512443a187bb5e84a0aa5160baa546
Fahad Abbas <fahad.dev@iu.com.pk>
2016-05-09 16:48:11 +0500

    modified : playe ratio is issue fixed

--- upload/player/CB_video_js/js/cb_vjs_custom.js
+++ upload/player/CB_video_js/js/cb_vjs_custom.js
@@ -1,12 +1,12 @@
 function new_player_height (argument) {
-	var player_ratio = 1.7;
+	var player_ratio = 1.77777;
 
 	var native_player = $(".video-js"); 
 	var embed_player = $("#cb_player");
 
 	var native_player_width = native_player.width();
 	var player_container_width = embed_player.width();
-	native_player_width
+	
 	var native_player_height  = native_player_width/player_ratio;
 	var embed_player_height = player_container_width/player_ratio;
 
