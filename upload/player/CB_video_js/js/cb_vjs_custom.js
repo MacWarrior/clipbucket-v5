0ffda8edb77d3ad8683b0eca807707d4a5a87f67
Fahad Abbas <fahad.dev@iu.com.pk>
2016-05-09 16:25:51 +0500

    modified : embed vidoe size and img circle class issue has benn fixed

--- upload/player/CB_video_js/js/cb_vjs_custom.js
+++ upload/player/CB_video_js/js/cb_vjs_custom.js
@@ -1,18 +1,27 @@
-/*function player_height(){var b=$(".video-js").width(),c=b*(9/16);return $(".video-js").css("height",c+"px"),c}$(window).on("resize",function(){player_height()}),$(document).ready(function(){player_height(),$(".vjs-cb-logo-cont").attr("style","display: block !important"),$(".vjs-cb-logo-cont").css({"float":"right",width:"3.5em",height:"2.5em",margin:"7px 0 0 0",cursor:"pointer"}),$(".vjs-cb-no-logo").css({height:"0.1px",margin:"7px 0 0 0"})});*/
 function new_player_height (argument) {
-	var w = $(".video-js").width();
-	var h = w*(9/16);
-	console.log(h);
-	console.log(w);
-	$(".video-js").css("height",h+"px");
+	var player_ratio = 1.7;
+
+	var native_player = $(".video-js"); 
+	var embed_player = $("#cb_player");
+
+	var native_player_width = native_player.width();
+	var player_container_width = embed_player.width();
+	native_player_width
+	var native_player_height  = native_player_width/player_ratio;
+	var embed_player_height = player_container_width/player_ratio;
+
+	native_player.css("height",native_player_height+"px");
+	embed_player.css("height",embed_player_height+"px");
 }
-//new_player_height();
+
 $(window).load(function(){
 	new_player_height();
 });
+
 $(document).ready(function() {
 	new_player_height();	
 });
+
 $(window).resize(function(event) {
 	new_player_height();
 });
