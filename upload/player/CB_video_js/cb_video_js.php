099e50f0d7dadd9f9d608ecf1a3d901d1383e54d
Fahad Abbas <fahad.dev@iu.com.pk>
2016-05-16 15:49:51 +0500

    modified : video js default resoluion issue fixed

--- upload/player/CB_video_js/cb_video_js.php
+++ upload/player/CB_video_js/cb_video_js.php
@@ -3,7 +3,7 @@
 	Player Name: cb video js 1.0 STABLE
 	Description: New Official cb video js player 
 	Author: Fahad Abbas
-	ClipBucket Version: 2.7
+	ClipBucket Version: 2.8.1
 
  
  * @Author : Arslan Hassan
@@ -51,11 +51,59 @@ if (!function_exists('cb_video_js'))
 		return true;
 	}
 
+	/*
+	* This Function is written to base64 encode file fo cb logo
+	*/
 	function cb_logo()
 	{
 		$l_details = BASEURL.'/images/icons/country/hp-cb.png';
 		$l_convert = base64_encode(file_get_contents($l_details));
 		return $l_convert;
+	}
+
+	/*
+	* This Function is written to get qulaity of current file
+	*/
+	function get_cbvjs_quality($src){
+		
+		$quality = explode('-', $src);
+	    $quality = end($quality);
+	    $quality = explode('.',$quality);
+	    $quality = $quality[0];
+	    return $quality;
+	}
+
+
+	/*
+	* This Function is written to set default resolution for cb_vjs_player
+	*/
+	function get_cbvjs_quality_type($video_files){
+		if ($video_files){
+			$one_file = get_cbvjs_quality($video_files[0]);
+			if (is_numeric($one_file)){
+				$cb_combo_res = True;
+			}else{
+				$cb_combo_res = False;
+			}
+
+			if ($cb_combo_res){
+				foreach ($video_files as $key => $file) {
+					$res[] = get_cbvjs_quality($file);
+				}
+				$all_res = $res;
+				if (in_array('360', $all_res)){
+					$quality = '360';
+				}else{
+					$quality = 'low';
+				}
+			}else{
+				$quality = "low";
+			}
+			return $quality;	
+		}else{
+			return False;
+		}
+		
 	}
 
 	register_actions_play_video('cb_video_js');
