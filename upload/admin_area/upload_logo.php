bf272091357eb2b0fecbc162a884994d3b1d597c
sikanderzel <sikander.zeliant@gmail.com>
2016-06-07 15:29:57 +0500

    use of functions

--- upload/admin_area/upload_logo.php
+++ upload/admin_area/upload_logo.php
@@ -9,53 +9,14 @@ if(!defined('MAIN_PAGE')){
 if(!defined('SUB_PAGE')){
 	define('SUB_PAGE', 'Update Logo');
 }
-$target_dir = STYLES_DIR."/cb_28/theme/images/";
+
 $source = BASEURL.'/styles/cb_28/theme/images/logo.png';
 
 // Upload and Rename File
 
-if (isset($_POST['submit']))
-{
-
-	$filename = $_FILES["fileToUpload"]["name"]; 
-	$file_basename = basename($filename,".png"); 
-	$file_ext = pathinfo($filename, PATHINFO_EXTENSION);
-	$filesize = $_FILES["fileToUpload"]["size"];
-	$allowed_file_types = array('png');	
-
-	if (in_array($file_ext,$allowed_file_types) && ($filesize < 4000000))
-	{	
-		// Rename file
-		$newfilename = 'logo.' . $file_ext;
-		unlink($target_dir."logo.png");
-		if (file_exists($target_dir . $newfilename))
-		{
-			// file already exists error
-				e(lang("You have already uploaded this file."),"e");
-		}
-		else
-		{		
-
-			move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_dir . $newfilename);	
-			e(lang("File uploaded successfully."),"m");
-		}
-	}
-	elseif (empty($file_basename))
-	{	
-		// file selection error
-		e(lang("Please select a file to upload."),"m");
-	} 
-	elseif ($filesize > 4000000)
-	{	
-		// file size error
-		e(lang("The file you are trying to upload is too large."),"e");
-	}
-	else
-	{
-
-		e(lang("Only these file typs are allowed for upload: ".implode(', ',$allowed_file_types)),"e");
-		unlink($_FILES["fileToUpload"]["tmp_name"]);
-	}
+if (isset($_POST['submit'])) {
+	// function used to upload site logo.
+	upload_logo() ;
 }
 
 assign('source',$source);
