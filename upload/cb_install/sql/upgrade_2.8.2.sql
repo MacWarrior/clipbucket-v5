e370c575b25fc00db955b6a61c6fd52f498e37c1
Fahad Abbas <fahad.dev@iu.com.pk>
2016-11-02 10:59:07 +0500

    modified : column added in videos table

--- upload/cb_install/sql/upgrade_2.8.2.sql
+++ upload/cb_install/sql/upgrade_2.8.2.sql
@@ -1,3 +1,5 @@
 -- Addition for 2.8.2
 INSERT INTO `{tbl_prefix}config` (`configid`, `name`, `value`) VALUES (NULL, 'store_guest_session', 'no');
 INSERT INTO `{tbl_prefix}config` (`configid`, `name`, `value`) VALUES (NULL, 'pseudostreaming', 'yes');
+
+ALTER TABLE `{tbl_prefix}video` ADD `re_conv_status` text(33) NOT NULL;
