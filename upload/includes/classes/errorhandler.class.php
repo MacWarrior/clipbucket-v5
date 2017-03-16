2fc02e4718830b643e9b9b5685c0ec3830cb436d
Fahad Abbas <adventivepk@gmail.com>
2017-03-16 19:38:58 +0500

    modified : error handler class issue fixed

--- upload/includes/classes/errorhandler.class.php
+++ upload/includes/classes/errorhandler.class.php
@@ -26,7 +26,7 @@ class errorhandler extends ClipBucket {
 		//if id is set, error will be generated from error message list
 		if(!$ignore_cb_errors)
 			$this->error_list[] = $message;
-			$this->error_list['all_errors']['user_error']['critical_priority'][] = $message;
+			//$this->error_list['all_errors']['user_error']['critical_priority'][] = $message;
 	}
 
 	
@@ -37,7 +37,7 @@ class errorhandler extends ClipBucket {
 	private function add_warning($message=NULL,$id=NULL) {
 		$this->warning_list[] = $message;
 		$this->user_errors['medium_priority'][] = $message;
-		$this->error_list['all_errors']['user_error']['medium_priority'][] = $message;
+		//$this->error_list['all_errors']['user_error']['medium_priority'][] = $message;
 	}
 	
 	/**
@@ -163,7 +163,7 @@ class errorhandler extends ClipBucket {
 	*/
 
 	private function addAll($error, $state, $type) {
-		return $this->error_list['all_errors'][$type][$state][] = $error;
+		//return $this->error_list['all_errors'][$type][$state][] = $error;
 	}
 
 	/**
