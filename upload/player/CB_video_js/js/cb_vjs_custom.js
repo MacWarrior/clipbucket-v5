9b022d4383a293efd80d7f57126c9a432678eab1
Fahad Abbas <adventivepk@gmail.com>
2017-03-16 12:55:33 +0500

    modified : video player container made dynamic

--- upload/player/CB_video_js/js/cb_vjs_custom.js
+++ upload/player/CB_video_js/js/cb_vjs_custom.js
@@ -1,8 +1,8 @@
-function new_player_height (argument) {
+function new_player_height (videoid) {
 	var player_ratio = 1.77777;
 
-	var native_player = $(".video-js"); 
-	var embed_player = $("#cb_player");
+	var native_player = $(".cb_video_js_"+videoid+"-dimensions"); 
+	var embed_player = $("#cb_player_"+videoid);
 
 	var native_player_width = native_player.width();
 	var player_container_width = embed_player.width();
@@ -13,22 +13,3 @@ function new_player_height (argument) {
 	native_player.css("height",native_player_height+"px");
 	embed_player.css("height",embed_player_height+"px");
 }
-
-$(window).load(function(){
-	new_player_height();
-});
-
-$(document).ready(function() {
-	new_player_height();
-	$(".video-js").on("contextmenu",function() {
-       return false;
-    }); 	
-});
-
-$(window).resize(function(event) {
-	new_player_height();
-});
-
-$(document).ready(function(){
-    
-});
