2322fe7e6bf966fa90534254f94cd65db5f342ef
Awais-cb <awaisfiazit@gmail.com>
2017-05-16 13:14:26 +0500

    Modified:Sql syntax error fixed

--- upload/cb_install/sql/upgrade_2.8.3.sql
+++ upload/cb_install/sql/upgrade_2.8.3.sql
@@ -11,6 +11,6 @@ INSERT INTO `{tbl_prefix}user_permissions` (
 `permission_code` ,
 `permission_desc` ,
 `permission_default`
-)VALUES ((
+)VALUES(
 NULL , '2', 'Allow create collection', 'allow_create_collection', 'Allow users to create collection', 'yes'), (
 NULL , '2', 'Allow create playlist', 'allow_create_playlist', 'Allow users to create playlist', 'yes');
