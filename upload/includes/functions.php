e0777a60cb36d58a5e8f46045c33014c123a78d1
Awais-cb <awaisfiazit@gmail.com>
2017-05-29 11:12:16 +0500

    Modified :added timeout limit if there is no response from curl resquest so that it won't disturb browsing upon cb

--- upload/includes/functions.php
+++ upload/includes/functions.php
@@ -3160,6 +3160,7 @@
 				'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2) Gecko/20070219 Firefox/3.0.0.2');
 				curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 				curl_setopt($ch,CURLOPT_FOLLOWLOCATION,true);
+				curl_setopt($ch, CURLOPT_TIMEOUT_MS, 600);
 				$contents = curl_exec($ch);
 				curl_close($ch);
 				
