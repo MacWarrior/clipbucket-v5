34ffba3ed846750640040b1873b50d95eb5e8367
MacWarrior <macwarrior94@gmail.com>
2017-12-05 21:18:16 +0100

    Improve mass upload : fix directory with special chars, optimization

--- upload/includes/classes/mass_upload.class.php
+++ upload/includes/classes/mass_upload.class.php
@@ -13,56 +13,58 @@
 
 		function get_video_files_list_clear()
 		{
-			return $this->get_video_files_list( true);
+			return $this->get_video_files_list(true);
 		}
 
-		function get_video_files_list($listonly = false, $dir = MASS_UPLOAD_DIR, &$count = 0)
+		function get_video_files_list($listonly = false, $dir = MASS_UPLOAD_DIR)
 		{
 			require_once(BASEDIR.'/includes/classes/conversion/ffmpeg.class.php');
 			$allowed_exts = get_vid_extensions();
-			$FILES = glob($dir.$this->dirsep.'*');
+			$FILES = scandir($dir);
 			$FILE_LIST = array();
 
-			foreach($FILES as $file)
+			foreach($FILES as $filename)
 			{
-				$filename = substr( $file, ( strrpos( $file, $this->dirsep ) +1 ));
-				if( is_dir($file) )
+				if(in_array($filename,array(".","..")))
+					continue;
+
+				$filepath = $dir.$this->dirsep.$filename;
+
+				if( is_dir($filepath) )
 				{
-					$new_files = $this->get_video_files_list($listonly, $file, $count);
+					$new_files = $this->get_video_files_list($listonly, $filepath);
+
 					if($new_files)
 					{
 						if( $listonly )
 						{
 							$FILE_LIST = array_merge($FILE_LIST, $new_files);
 						} else {
-							$FILE_LIST[$file]['dirname'] = $filename;
-							$FILE_LIST[$file]['files'] = $new_files;
+							$FILE_LIST[$filepath]['dirname'] = $filename;
+							$FILE_LIST[$filepath]['files'] = $new_files;
 						}
 					}
 					continue;
 				}
 
-				if( is_file($file) )
+				if( is_file($filepath) )
 				{
 					$file_extension = getext($filename);
 
 					if(in_array($file_extension, $allowed_exts))
 					{
 						$video_file = array();
-						$video_file['path']			= substr( $file, 0, ( strrpos( $file, $this->dirsep ) +1 ) );
+						$video_file['path']			= $dir.$this->dirsep;
 						$video_file['file']			= $filename;
-						$video_file['title']		= getName($filename);
-						$video_file['description']	= getName($filename);
-						$video_file['tags']			= gentags(str_replace(" ",",",getName($filename)));
-						$video_file['size']			= formatfilesize( filesize($file) );
-						$video_file['date']			= date('Y-m-d G:i:s', filemtime($file) );
-						$video_file['count']		= $count;
-						if( $tracks = FFMpeg::get_video_tracks($file) )
+						$video_file['title']		= $filename;
+						$video_file['description']	= $filename;
+						$video_file['tags']			= gentags(str_replace(" ",",",$filename));
+						$video_file['size']			= formatfilesize( filesize($filepath) );
+						if( $tracks = FFMpeg::get_video_tracks($filepath) )
 							$video_file['tracks'] 	= $tracks;
-						$video_file = array_merge($video_file, FFMpeg::get_video_basic_infos($file));
+						$video_file = array_merge($video_file, FFMpeg::get_video_basic_infos($filepath));
 
 						$FILE_LIST[] = $video_file;
-						$count++;
 					}
 				}
 			}
