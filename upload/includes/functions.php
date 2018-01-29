54eed43f55a49657fc2a12f8bb41db51f5cfd518
ASponch <adrien.ponchelet@u-picardie.fr>
2018-01-29 10:42:26 +0100

    modified:video cookie lifetime if video duration is bigger than one hour

--- upload/includes/functions.php
+++ upload/includes/functions.php
@@ -2181,8 +2181,11 @@
 			{	
 				if(!isset($_COOKIE['video_'.$id])) {
 					$currentTime = time();
+					$vdetails = get_video_details($id);
+					// Cookie life time at least 1 hour else if video duration is bigger set at video time.
+					$cookieTime = ($vdetails['duration'] > 3600) ? $vdetails['duration'] : $cookieTime = 3600;
 					$db->update(tbl("video"),array("views", "last_viewed"),array("|f|views+1",$currentTime)," videoid='$id' OR videokey='$id'");
-					setcookie('video_'.$id,'watched',time()+3600);
+					setcookie('video_'.$id,'watched',time()+$cookieTime);
 				}
 			}
 			break;
