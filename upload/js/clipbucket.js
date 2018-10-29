bf38c715cc5f55c809daeb16b55a69a10a2d9a32
MacWarrior <macwarrior94@gmail.com>
2018-10-29 21:09:39 +0100

    BO : Flagged Users Manager : Fix check all

--- upload/js/clipbucket.js
+++ upload/js/clipbucket.js
@@ -1530,7 +1530,7 @@
 		};
 
 		this.checkUncheckAll = function(theElement) {
-		     var theForm = theElemeform, z = 0;
+		     var theForm = theElement.form, z = 0;
 			 
 				for(z=0; z<theForm.length;z++){
 					if(theForm[z].type == 'checkbox' && theForm[z].name != 'checkall'){
