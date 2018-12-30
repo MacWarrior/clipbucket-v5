f546f193e41bf965e4ac9e8eb4e12b355e6a08af
MacWarrior <macwarrior94@gmail.com>
2018-12-30 11:42:15 +0100

    Fix typo

--- upload/cb_install/sql/upgrade_5.1.sql
+++ upload/cb_install/sql/upgrade_5.1.sql
@@ -5,7 +5,7 @@ UPDATE `{tbl_prefix}config` SET name = 'allowed_video_types' WHERE name = 'allow
 INSERT INTO `{tbl_prefix}config`(`name`, `value`) VALUES
 	('allowed_photo_types', 'jpg,jpeg,png');
 
-DELETE FROM `{tbl_prefix}config` WHERE name = 'users_items_subscibers';
+DELETE FROM `{tbl_prefix}config` WHERE name = 'users_items_subscribers';
 
 UPDATE `{tbl_prefix}config` SET value = CONCAT(value, ',mkv') WHERE name = 'allowed_video_types' AND value NOT LIKE '%mkv%';
 UPDATE `{tbl_prefix}config` SET value = CONCAT(value, ',webm') WHERE name = 'allowed_video_types' AND value NOT LIKE '%webm%';
