b6dfa272a2afad08ef0f0fda786d9ac9baf4fb95
Fahad Abbas <fahad.dev@iu.com.pk>
2019-01-02 16:43:16 +0500

    added : elastic search plugin added , now search Elastic :)

---
+++ upload/plugins/cb_elastic_search/admin/index_data.php
@@ -0,0 +1,20 @@
+<?php
+/* 
+ ***************************************************************
+ | Copyright (c) 2007-2010 Clip-Bucket.com. All rights reserved.
+ | @ Author : ArslanHassan										
+ | @ Software : ClipBucket , © PHPBucket.com					
+ ****************************************************************
+*/
+
+if(!defined('IN_CLIPBUCKET'))
+	exit('Invalid access');
+
+
+
+
+
+$template = 'index_data.html';
+template_files($template,CB_ES_ADMIN_DIR);
+
+?>
