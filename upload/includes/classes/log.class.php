57d625567d567738f1a03729447bd4685c5ff5e2
MacWarrior <macwarrior94@gmail.com>
2019-08-18 12:23:40 +0200

    Cleanup and fix log saving

--- upload/includes/classes/log.class.php
+++ upload/includes/classes/log.class.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * This class is used to keep details of each action done on a website using Clipbucket
  * this will manage log of each possible action 
@@ -22,11 +21,8 @@
  * - add_favorite
  */
 
-
 class CBLogs
 {
-	
-	
 	/**
 	 * Function used to insert log
 	 * @param VARCHAR $type, type of action
@@ -35,25 +31,35 @@ class CBLogs
 	function insert($type,$details_array)
 	{
 		global $db,$userquery;
-		$a = $details_array;
 		$ip = $_SERVER['REMOTE_ADDR'];
 
-		$agent = $_SERVER['HTTP_USER_AGENT'];
-		$userid = getArrayValue($a, 'userid');
-		$username = $a['username'];
-		$useremail = getArrayValue($a, 'useremail');
-		$userlevel = getArrayValue($a, 'userlevel');
+		$userid = getArrayValue($details_array, 'userid');
+		if( !$userid ){
+			$userid = $userquery->udetails['userid'];
+		}
 
-		$action_obj_id = getArrayValue($a, 'action_obj_id');
-		$action_done_id = getArrayValue($a, 'action_done_id');
+		$username = getArrayValue($details_array, 'username');
+		if( !$username ){
+			$username = $userquery->udetails['username'];
+		}
 
-		$userid = $userid ? $userid : $userquery->udetails['userid'];
-		$username = $username ? $username : $userquery->udetails['username'];
-		$useremail = $useremail ? $useremail : $userquery->udetails['email'];
-		$userlevel = $userlevel ? $userlevel : getArrayValue($userquery->udetails, 'level');
+		$useremail = getArrayValue($details_array, 'useremail');
+		if( !$useremail ){
+			$useremail = $userquery->udetails['email'];
+		}
 
-		$success = $a['success'];
-		$details = getArrayValue($a, 'details');
+		$userlevel = getArrayValue($details_array, 'userlevel');
+		if( !$userlevel ){
+			$userlevel = getArrayValue($userquery->udetails, 'level');
+		}
+
+		$action_obj_id = getArrayValue($details_array, 'action_obj_id');
+		$action_done_id = getArrayValue($details_array, 'action_done_id');
+		if( !is_numeric($action_done_id) ){
+			$action_done_id = 0;
+		}
+		$success = getArrayValue($details_array, 'success');
+		$details = getArrayValue($details_array, 'details');
 
 		$db->insert(tbl('action_log'),
             array(
@@ -83,27 +89,5 @@ class CBLogs
                 $action_done_id
             )
 		);
-					  
-	 }
-
-	 function get_local_ipv4() {
-  		$out = split(PHP_EOL,shell_exec("/sbin/ifconfig"));
-		$local_addrs = array();
-		$ifname = 'unknown';
-		foreach($out as $str) {
-		   $matches = array();
-		    if(preg_match('/^([a-z0-9]+)(:\d{1,2})?(\s)+Link/',$str,$matches)) {
-		      $ifname = $matches[1];
-		      if(strlen($matches[2])>0) {
-		        $ifname .= $matches[2];
-		      }
-		    } elseif(preg_match('/inet addr:((?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:[.](?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3})\s/',$str,$matches)) {
-		      $local_addrs[$ifname] = $matches[1];
-		    }
-		  }
-		  return $local_addrs;
-		}
-	 
+	}
 }
-
-?>
