9dc8cace3332f21236901602b1b918c31facab94
MacWarrior <macwarrior94@gmail.com>
2019-08-18 15:51:06 +0200

    Remove old upgrade SQL

--- upload/cb_install/sql/upgrade_2.7.3.sql
+++
@@ -1,3 +0,0 @@
--- update template adn player query on cb update
-UPDATE `{tbl_prefix}config` SET value = 'cb_27' WHERE name = 'template_dir';
-UPDATE `{tbl_prefix}config` SET value = 'html5_player.php' WHERE name = 'player_file';
--- upload/cb_install/sql/upgrade_2.7.6.sql
+++
@@ -1,4 +0,0 @@
--- Addition for 2.7.6
-UPDATE `{tbl_prefix}config` SET value = 'cb_video_js.php' WHERE name = 'player_file';
-UPDATE `{tbl_prefix}config` SET value = 'cb_video_js' WHERE name = 'player_dir';
-
