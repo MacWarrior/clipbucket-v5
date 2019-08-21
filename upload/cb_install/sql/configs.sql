d6fb0040812dd63da66be6bcdd1b0187ac817ea1
MacWarrior <macwarrior94@gmail.com>
2019-08-21 15:38:14 +0200

    Fix installation configs SQL, add french language pack

--- upload/cb_install/sql/configs.sql
+++ upload/cb_install/sql/configs.sql
@@ -261,3 +261,12 @@ INSERT INTO `{tbl_prefix}config` (`configid`, `name`, `value`) VALUES
 	(NULL, 'logo_name', ''),
 	(NULL, 'favicon_name', ''),
 	(NULL, 'comment_per_page', '10');
+
+INSERT INTO `{tbl_prefix}languages` (`language_id`, `language_code`, `language_name`, `language_regex`, `language_active`, `language_default`) VALUES
+	(1, 'en', 'English', '/^en/i', 'yes', 'yes'),
+	(2, 'fr', 'French', '/^fr/i', 'yes', 'no');
+
+INSERT INTO `{tbl_prefix}validation_re` (`re_id`, `re_name`, `re_code`, `re_syntax`) VALUES
+	(1, 'Username', 'username', '^^[a-zA-Z0-9_]+$'),
+	(2, 'Email', 'email', '^[_a-z0-9-]+(\\.[_a-z0-9-]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)*(\\.[a-z]{2,10})$'),
+	(3, 'Field Text', 'field_text', '^^[_a-z0-9-]+$');
