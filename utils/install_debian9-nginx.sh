22facf6a579e82b157b1b68f33f7cfc4bd1e2995
MacWarrior <macwarrior94@gmail.com>
2020-04-12 11:51:40 +0200

    Update Nginx SEO url compatibility

--- utils/install_debian9-nginx.sh
+++ utils/install_debian9-nginx.sh
@@ -47,137 +47,145 @@ echo -ne "Configuring Nginx Vhost..."
 rm -f /etc/nginx/sites-enabled/default
 cat << 'EOF' > /etc/nginx/sites-available/001-clipbucket
 server {
-        listen 80;
-        server_name clipbucket.local;
+    listen 80;
+    server_name clipbucket.local;
 
-        root /home/http/clipbucket/upload/;
-        index index.php;
+    root /home/http/clipbucket/upload/;
+    index index.php;
 
-        # set expiration of assets to MAX for caching
-        location ~* \.(ico|css|js)(\?[0-9]+)?$ {
-                expires max;
-                log_not_found off;
-        }
+    # set expiration of assets to MAX for caching
+    location ~* \.(ico|css|js)(\?[0-9]+)?$ {
+        expires max;
+        log_not_found off;
+    }
 
-        location ~* \.php$ {
-                fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;
-                fastcgi_index index.php;
-                fastcgi_split_path_info ^(.+\.php)(.*)$;
-                include fastcgi_params;
-                fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-        }
+    location ~* \.php$ {
+        fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;
+        fastcgi_index index.php;
+        fastcgi_split_path_info ^(.+\.php)(.*)$;
+        include fastcgi_params;
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+    }
 
-		location /video/ {
-			rewrite ^/video/(.*)/(.*) /watch_video.php?v=$1&$query_string last;
-			rewrite ^/video/([0-9]+)_(.*) /watch_video.php?v=$1&$query_string last;
+	location / {
+		if ($query_string ~ "mosConfig_[a-zA-Z_]{1,21}(=|\%3D)"){
+			rewrite ^/([^.]*)/?$ /index.php last;
 		}
+		rewrite ^/(.*)_v([0-9]+) /watch_video.php?v=$2&$query_string last;
+		rewrite ^/([a-zA-Z0-9-]+)/?$ /view_channel.php?uid=$1&seo_diret=yes last;
+    }
 
-		location /videos/ {
-			rewrite ^/videos/(.*)/(.*)/(.*)/(.*)/(.*) /videos.php?cat=$1&sort=$3&time=$4&page=$5&seo_cat_name=$2 last;
-			rewrite ^/videos/([0-9]+) /videos.php?page=$1 last;
-			rewrite ^/videos/?$ /videos.php?$query_string last;
-		}
+    location /video/ {
+        rewrite ^/video/(.*)/(.*) /watch_video.php?v=$1&$query_string last;
+        rewrite ^/video/([0-9]+)_(.*) /watch_video.php?v=$1&$query_string last;
+    }
 
-		location /channels/ {
-			rewrite ^/channels/(.*)/(.*)/(.*)/(.*)/(.*) /channels.php?cat=$1&sort=$3&time=$4&page=$5&seo_cat_name=$2 last;
-			rewrite ^/channels/([0-9]+) /channels.php?page=$1 last;
-			rewrite ^/channels/?$ /channels.php last;
-		}
+    location /videos/ {
+        rewrite ^/videos/(.*)/(.*)/(.*)/(.*)/(.*) /videos.php?cat=$1&sort=$3&time=$4&page=$5&seo_cat_name=$2 last;
+        rewrite ^/videos/([0-9]+) /videos.php?page=$1 last;
+        rewrite ^/videos/?$ /videos.php?$query_string last;
+    }
 
-		location /members/ {
-			rewrite ^/members/?$ /channels.php last;
-		}
+    location /channels/ {
+        rewrite ^/channels/(.*)/(.*)/(.*)/(.*)/(.*) /channels.php?cat=$1&sort=$3&time=$4&page=$5&seo_cat_name=$2 last;
+        rewrite ^/channels/([0-9]+) /channels.php?page=$1 last;
+        rewrite ^/channels/?$ /channels.php last;
+    }
 
-		location /users/ {
-			rewrite ^/users/?$ /channels.php last;
-		}
+    location /members/ {
+        rewrite ^/members/?$ /channels.php last;
+    }
 
-		location /user/ {
-			rewrite ^/user/(.*) /view_channel.php?user=$1 last;
-		}
+    location /users/ {
+        rewrite ^/users/?$ /channels.php last;
+    }
 
-		location /channel/ {
-			rewrite ^/channel/(.*) /view_channel.php?user=$1 last;
-		}
+    location /user/ {
+        rewrite ^/user/(.*) /view_channel.php?user=$1 last;
+    }
 
-		location /my_account {
-			rewrite ^/my_account /myaccount.php last;
-		}
+    location /channel/ {
+        rewrite ^/channel/(.*) /view_channel.php?user=$1 last;
+    }
 
-		location /page/ {
-			rewrite ^/page/([0-9]+)/(.*) /view_page.php?pid=$1 last;
-		}
+    location /my_account {
+        rewrite ^/my_account /myaccount.php last;
+    }
 
-		location /search/ {
-			rewrite ^/search/result/?$ /search_result.php last;
-		}
+    location /page/ {
+        rewrite ^/page/([0-9]+)/(.*) /view_page.php?pid=$1 last;
+    }
 
-		location /upload/ {
-			rewrite ^/upload/?$ /upload.php last;
-		}
+    location /search/ {
+        rewrite ^/search/result/?$ /search_result.php last;
+    }
 
-		location /contact/ {
-			rewrite ^/contact/?$ /contact.php last;
-		}
+    location /upload/ {
+        rewrite ^/upload/?$ /upload.php last;
+    }
 
-		location /categories/ {
-			rewrite ^/categories/?$ /categories.php last;
-		}
+    location /contact/ {
+        rewrite ^/contact/?$ /contact.php last;
+    }
 
-		location /collections/ {
-			rewrite ^/collections/(.*)/(.*)/(.*)/(.*)/(.*) /collections.php?cat=$1&sort=$3&time=$4&page=$5&seo_cat_name=$2 last;
-			rewrite ^/collections/([0-9]+) /collections.php?page=$1 last;
-			rewrite ^/collections/?$ /collections.php last;
-		}
+    location /categories/ {
+        rewrite ^/categories/?$ /categories.php last;
+    }
 
-		location /photos/ {
-			rewrite ^/photos/(.*)/(.*)/(.*)/(.*)/(.*) /photos.php?cat=$1&sort=$3&time=$4&page=$5&seo_cat_name=$2 last;
-			rewrite ^/photos/([0-9]+) /photos.php?page=$1 last;
-			rewrite ^/photos/?$ /photos.php last;
-		}
+    location /collections/ {
+        rewrite ^/collections/(.*)/(.*)/(.*)/(.*)/(.*) /collections.php?cat=$1&sort=$3&time=$4&page=$5&seo_cat_name=$2 last;
+        rewrite ^/collections/([0-9]+) /collections.php?page=$1 last;
+        rewrite ^/collections/?$ /collections.php last;
+    }
 
-		location /collection/ {
-			rewrite ^/collection/(.*)/(.*)/(.*) /view_collection.php?cid=$1&type=$2&page=$3 last;
-		}
+    location /photos/ {
+        rewrite ^/photos/(.*)/(.*)/(.*)/(.*)/(.*) /photos.php?cat=$1&sort=$3&time=$4&page=$5&seo_cat_name=$2 last;
+        rewrite ^/photos/([0-9]+) /photos.php?page=$1 last;
+        rewrite ^/photos/?$ /photos.php last;
+    }
 
-		location /item/ {
-			rewrite ^/item/(.*)/(.*)/(.*)/(.*) /view_item.php?item=$3&type=$1&collection=$2 last;
-		}
+    location /collection/ {
+        rewrite ^/collection/(.*)/(.*)/(.*) /view_collection.php?cid=$1&type=$2&page=$3 last;
+    }
 
-		location /photo_upload/ {
-			rewrite ^/photo_upload/(.*) /photo_upload.php?collection=$1 last;
-			rewrite ^/photo_upload/?$ /photo_upload.php last;
-		}
+    location /item/ {
+        rewrite ^/item/(.*)/(.*)/(.*)/(.*) /view_item.php?item=$3&type=$1&collection=$2 last;
+    }
 
-		location = /sitemap.xml {
-			rewrite ^(.*)$ /sitemap.php last;
-		}
+    location /photo_upload/ {
+        rewrite ^/photo_upload/(.*) /photo_upload.php?collection=$1 last;
+        rewrite ^/photo_upload/?$ /photo_upload.php last;
+    }
 
-		location /signup/ {
-			rewrite ^/signup/?$ /signup.php last;
-		}
+    location = /sitemap.xml {
+        rewrite ^(.*)$ /sitemap.php last;
+    }
 
-		location /manage_audios/ {
-			rewrite ^/manage_audios/?$ /module.php?s=audios&p=manage last;
-			rewrite ^/manage_audios/delete/(.*) /module.php?s=audios&p=manage&del_beat=$1 last;
-			rewrite ^/manage_audios/edit/audio/(.*) /module.php?s=audios&p=edit&audio=$1 last;
-		}
+    location /signup/ {
+        rewrite ^/signup/?$ /signup.php last;
+    }
 
-		location /audios/ {
-			rewrite ^/audios/?$ /module.php?s=audios&p=list last;
-		}
+    location /manage_audios/ {
+        rewrite ^/manage_audios/?$ /module.php?s=audios&p=manage last;
+        rewrite ^/manage_audios/delete/(.*) /module.php?s=audios&p=manage&del_beat=$1 last;
+        rewrite ^/manage_audios/edit/audio/(.*) /module.php?s=audios&p=edit&audio=$1 last;
+    }
 
-		location /rss/ {
-			rewrite ^/rss/([a-zA-Z0-9].+)$ /rss.php?mode=$1&$query_string last;
-		}
+    location /audios/ {
+        rewrite ^/audios/?$ /module.php?s=audios&p=list last;
+    }
 
-		location /list/ {
-			rewrite ^/list/([0-9]+)/(.*)?$ /view_playlist.php?list_id=$1 last;
-		}
+    location /rss/ {
+        rewrite ^/rss/([a-zA-Z0-9].+)$ /rss.php?mode=$1&$query_string last;
+    }
 
-		location ~ /rss$ {
-			try_files $uri /rss.php;
-		}
+    location /list/ {
+        rewrite ^/list/([0-9]+)/(.*)?$ /view_playlist.php?list_id=$1 last;
+    }
+
+    location ~ /rss$ {
+        try_files $uri /rss.php;
+    }
 }
 EOF
 
