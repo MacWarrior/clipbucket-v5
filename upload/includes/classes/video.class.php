58cec30c66c451c000e5a56880c87e27c7c760fa
MacWarrior <macwarrior94@gmail.com>
2020-05-05 19:58:05 +0200

    Cleanup

--- upload/includes/classes/video.class.php
+++ upload/includes/classes/video.class.php
@@ -1225,8 +1225,13 @@ class CBvideo extends CBCategory
 		{
 			$embed_code = '<iframe width="'.config('embed_player_width').'" height="'.config('embed_player_height').'" ';
 			$embed_code .= 'src="'.BASEURL.'/player/embed_player.php?vid='.$vdetails['videokey'].'&width='.
-			config('embed_player_width').'&height='.config('embed_player_height').
-			'&autoplay='.config('autoplay_embed').'" frameborder="0" allowfullscreen></iframe>';
+			config('embed_player_width').'&height='.config('embed_player_height');
+
+			if( config('autoplay_embed') == 'yes' ){
+                $embed_code .= '&autoplay=yes';
+            }
+
+            $embed_code .= '" frameborder="0" allowfullscreen></iframe>';
 		}
 		
 		if(!$embed_code)
