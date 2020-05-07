c68aa9c9f257225a92536b0fe8236612da6b6530
MacWarrior <macwarrior94@gmail.com>
2020-05-07 08:40:39 +0200

    Admin : Update changelog display, cleanup

--- upload/admin_area/styles/cb_2014/theme/js/main.js
+++ upload/admin_area/styles/cb_2014/theme/js/main.js
@@ -1,73 +1,35 @@
-
-//like in my previous post, I will set the value of window.onload
-
-//equals to init and then define it separately
-
 window.onload = init;
 
 function init(){
 
     for (var i=0; i < localStorage.length; i++){
-
         var key = localStorage.key(i);
-
         if (key.substring(0, 6) === 'note'){ //will explain this
-
             var value = localStorage.getItem(key);
-
             addStickiesToPage(value); //we will create this one
-
         }
-
     }
 
-}
+    $('.changelog .arrow').click(function(){
+        if( $(this).parent().next().is(':visible') ){
+            $(this).parent().next().css('display','none');
+            $(this).removeClass('icon-angle-up').addClass('icon-angle-down');
+        } else {
+            $(this).parent().next().css('display','block');
+            $(this).removeClass('icon-angle-down').addClass('icon-angle-up');
+        }
+    });
 
-//
+}
 
 //this function will insert the sticky notes to the DOM
-
 //inside the <ul> element we created in the index.html file
-
 function addStickiesToPage(value){
-
     var stickies = document.getElementById("note-");
-
     var note = document.createElement("li");
-
     var span = document.createElement("span");
-
     span.setAttribute("class", "note");
-
     span.innerHTML = value;
-
     note.appendChild(span);
-
     stickies.appendChild(note);
-
-}
-
-
-function init(){
-
-    var button = document.getElementById("add_note");
-    //button.click = makenote; //we have to define this function
-
-    //continue the init function here as shown above by adding
-
-    //the for loop to get items from localStorage
-
-}
-
-
-function makenote(){
-
-    var value = document.getElementById("personalnote").value;
-
-    var key = "note_" + localStorage.length;
-
-    localStorage.setItem(key, value);
-
-    addStickiesToPage(value);
-
 }
