786d8e07cf9b7c75ad767630d672b5bd40a89ed5
MacWarrior <macwarrior94@gmail.com>
2021-05-19 22:55:40 +0200

    Miscellaneous : Update upgrade_from_original.sql

--- upload/cb_install/sql/upgrade_from_original.sql
+++ upload/cb_install/sql/upgrade_from_original.sql
@@ -499,3 +499,17 @@ ALTER TABLE `{tbl_prefix}playlists`
 	MODIFY COLUMN `first_item` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
 	MODIFY COLUMN `cover` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
 	MODIFY COLUMN `played` INT(255) NOT NULL DETAULT '0';
+
+-- upgrade_5.4.1.sql
+INSERT INTO `{tbl_prefix}config`(`name`, `value`) VALUES
+	('email_domain_restriction', '');
+
+ALTER TABLE `{tbl_prefix}video`
+	MODIFY COLUMN `videokey` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+	MODIFY COLUMN `video_password` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+	MODIFY COLUMN `video_users` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+	MODIFY COLUMN `userid` INT(11) NULL DEFAULT NULL,
+	MODIFY COLUMN `file_name` VARCHAR(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+	MODIFY COLUMN `tags` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+	MODIFY COLUMN `uploader_ip` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+	MODIFY COLUMN `file_directory` VARCHAR(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
