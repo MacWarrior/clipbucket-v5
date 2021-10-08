a1849f62b27505712fb3c6974b12a280be133e83
MacWarrior <macwarrior94@gmail.com>
2021-10-08 22:02:24 +0200

    Miscellaneous : Fix installation process, Cleanup code

--- upload/cb_install/index.php
+++ upload/cb_install/index.php
@@ -1,39 +1,42 @@
 <?php
-	define('THIS_PAGE','cb_install');
-	include('../includes/clipbucket.php');
- 
-	$modes = array(
-		'agreement',
-		'precheck',
-		'permission',
-		'database',
-		'dataimport',
-		'adminsettings',
-		'sitesettings',
-		'finish'
-	);
+define('THIS_PAGE','cb_install');
+include('../includes/clipbucket.php');
 
-	$mode = @$_POST['mode'];
+$modes = array(
+    'agreement',
+    'precheck',
+    'permission',
+    'database',
+    'dataimport',
+    'adminsettings',
+    'sitesettings',
+    'finish'
+);
 
-	if(!$mode || !in_array($mode,$modes)) {
-        $mode = 'agreement';
-	}
+$mode = $_POST['mode'] ?? false;
+error_log('mode : '.$mode);
+error_log('Request mode : '.$_REQUEST['mode']);
 
-	$configIncluded = false;
-	/**
-	* Clipbucket modes
-	* modes which requires clipbucket core files so installer
-	* function file does not create a conflict
-	*/
-	$cbarray = array('adminsettings','sitesettings','finish');
-	$baseDir = dirname(dirname(__FILE__));
+if(!$mode || !in_array($mode,$modes)) {
+    error_log('redirection to agreement');
+    $mode = 'agreement';
+}
 
-	if( in_array($mode,$cbarray) ) {
-		chdir('..');
-		$configIncluded = true;
-		require_once 'includes/config.inc.php';
-		chdir('cb_install');
-	}
+$configIncluded = false;
+/**
+* Clipbucket modes
+* modes which requires clipbucket core files so installer
+* function file does not create a conflict
+*/
+$cbarray = array('adminsettings','sitesettings','finish');
+$baseDir = dirname(dirname(__FILE__));
 
-	include('functions.php');
-	include('modes/body.php');
+if( in_array($mode,$cbarray) ) {
+    chdir('..');
+    $configIncluded = true;
+    require_once 'includes/config.inc.php';
+    chdir('cb_install');
+}
+
+include('functions.php');
+include('modes/body.php');
