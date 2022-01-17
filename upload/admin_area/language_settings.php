f72b6c0f8e202113e7169f2da1e7dab272eee92f
MacWarrior <macwarrior94@gmail.com>
2022-01-17 20:28:41 +0100

    Admin area : Fix default language selection #199 Miscellaneous : Update translations, Cleanup code

--- upload/admin_area/language_settings.php
+++ upload/admin_area/language_settings.php
@@ -1,139 +1,134 @@
 <?php
-	/*
-	 **************************************************************
-	 | Copyright (c) 2007-2010 Clip-Bucket.com. All rights reserved.
-	 | @ Author 	: ArslanHassan
-	 | @ Software 	: ClipBucket , © PHPBucket.com
-	 ***************************************************************
-	*/
-	require_once '../includes/admin_config.php';
-	$userquery->admin_login_check();
-	$userquery->login_check('web_config_access');
-	$pages->page_redir();
+require_once '../includes/admin_config.php';
 
-	/* Generating breadcrumb */
-	global $breadcrumb;
-	$breadcrumb[0] = array('title' => 'General Configurations', 'url' => '');
-	$breadcrumb[1] = array('title' => 'Language Settings', 'url' => ADMIN_BASEURL.'/language_settings.php');
+global $userquery,$pages,$Cbucket;
 
-	global $lang_obj;
-	$langData = $lang_obj->getPhrasesFromPack('en');
-	$langData['last_one'] = "aye";
-	assign("langData", $langData);
+$userquery->admin_login_check();
+$userquery->login_check('web_config_access');
+$pages->page_redir();
 
-	//Making Language Default
-	if(isset($_POST['make_default']))
-	{
-		$id = mysql_clean($_POST['make_default']);
-		$lang_obj->make_default($id);
-	}
+/* Generating breadcrumb */
+global $breadcrumb;
+$breadcrumb[0] = array('title' => 'General Configurations', 'url' => '');
+$breadcrumb[1] = array('title' => 'Language Settings', 'url' => ADMIN_BASEURL.'/language_settings.php');
 
-	//Making Language Default
-	if(isset($_GET['make_default']))
-	{
-		$id = mysql_clean($_GET['make_default']);
-		$lang_obj->make_default($id);
-	}
+global $lang_obj;
+$langData = $lang_obj->getPhrasesFromPack('en');
+$langData['last_one'] = 'aye';
+assign('langData', $langData);
 
-	//Importing language
-	if(isset($_POST['add_language']))
-	{
-		$lang_obj->import_lang();
-	}
+//Making Language Default
+if(isset($_POST['make_default'])) {
+    $id = mysql_clean($_POST['make_default']);
+    $lang_obj->make_default($id);
+}
 
-	//Removing Language
-	if(isset($_GET['delete']))
-	{
-		$id = mysql_clean($_GET['delete']);
-		$lang_obj->delete_lang($id);
-	}
+//Making Language Default
+if(isset($_GET['make_default'])) {
+    $id = mysql_clean($_GET['make_default']);
+    $lang_obj->make_default($id);
+}
 
-	//Updating Language
-	if(isset($_POST['update_language']))
-	{
-		$_POST['lang_id'] = $_GET['edit_language'];
-		$lang_obj->update_lang($_POST);
-	}
+//Importing language
+if(isset($_POST['add_language'])) {
+    $lang_obj->import_lang();
+}
 
-	//Downloading Language
-	if(isset($_GET['download']))
-		$lang_obj->export_lang_Json(mysql_clean($_GET['download']));
+//Removing Language
+if(isset($_GET['delete'])) {
+    $id = mysql_clean($_GET['delete']);
+    $lang_obj->delete_lang($id);
+}
 
-	//Downloading Language
-	if(isset($_GET['action']))
-		$lang_obj->action_lang($_GET['action'],mysql_clean($_GET['id']));
+//Updating Language
+if(isset($_POST['update_language'])) {
+    $_POST['lang_id'] = $_GET['edit_language'];
+    $lang_obj->update_lang($_POST);
+}
 
-	//Create package
-	if(isset($_GET['create_package']) && $lang_obj->createPack($_GET['create_package']))
-		e("Language pack has been re-created","m");
+//Downloading Language
+if(isset($_GET['download'])){
+    $lang_obj->export_lang_Json(mysql_clean($_GET['download']));
+}
 
-	//Create package
-	if(isset($_GET['recreate_from_pack']) && $lang_obj->updateFromPack($_GET['recreate_from_pack']))
-		e("Language database has been updated","m");
+//Downloading Language
+if(isset($_GET['action'])){
+    $lang_obj->action_lang($_GET['action'],mysql_clean($_GET['id']));
+}
 
-	if(isset($_POST['set_language']))
-	{
-		$ClientId = $_POST['client_id'];
-		$secertId = $_POST['sec_id'];
-		$c =strlen($ClientId);
-		$csec =strlen($secertId);
-		if($c < 10 || $csec < 10){
-			e("invalid keys");
-		} else {
-			$lang_obj->set_lang($ClientId,$secertId);
-			e("keys set","m");
-		}
-	}
+//Create package
+if(isset($_GET['create_package']) && $lang_obj->createPack($_GET['create_package'])){
+    e('Language pack has been re-created','m');
+}
 
-	//Get List Of Languages
-	assign('language_list',$lang_obj->get_langs());
-	Assign('msg',$msg);
+//Create package
+if(isset($_GET['recreate_from_pack']) && $lang_obj->updateFromPack($_GET['recreate_from_pack'])){
+    e('Language database has been updated','m');
+}
 
-	if($lang_obj->lang_exists(mysql_clean($_GET['edit_language'])))
-	{
-		assign('edit_lang','yes');
-		assign('lang_details',$lang_obj->lang_exists(mysql_clean($_GET['edit_language'])));
-		$edit_id = mysql_clean($_GET['edit_language']);
-		$limit = RESULTS;
+if(isset($_POST['set_language'])) {
+    $ClientId = $_POST['client_id'];
+    $secertId = $_POST['sec_id'];
+    $c =strlen($ClientId);
+    $csec =strlen($secertId);
+    if($c < 10 || $csec < 10){
+        e('invalid keys');
+    } else {
+        $lang_obj->set_lang($ClientId,$secertId);
+        e('keys set','m');
+    }
+}
 
-		$current_page = $_GET['page'] ;
-		$current_page = is_numeric($current_page) && $current_page>0 ? $current_page : 1 ;
+//Get List Of Languages
+assign('language_list',$lang_obj->get_langs());
+Assign('msg',$msg);
 
-		$curr_limit = ($current_page-1)*$limit .','.$limit;
+if($lang_obj->lang_exists(mysql_clean($_GET['edit_language']))) {
+    assign('edit_lang','yes');
+    assign('lang_details',$lang_obj->lang_exists(mysql_clean($_GET['edit_language'])));
+    $edit_id = mysql_clean($_GET['edit_language']);
+    $limit = RESULTS;
 
-		if(isset($_GET['search_phrase']))
-		{
-			$varname = mysql_clean($_GET['varname']);
-			$text = mysql_clean($_GET['text']);
+    $current_page = $_GET['page'] ;
+    $current_page = is_numeric($current_page) && $current_page>0 ? $current_page : 1 ;
 
-			if(!empty($varname))
-				$varname_query = "varname LIKE '%$varname%'";
-			if(!empty($text))
-				$text_query = "text LIKE '%$text%'";
+    $curr_limit = ($current_page-1)*$limit .','.$limit;
 
-			if(!empty($text_query) || !empty($varname_query))
-			{
-				if(!empty($text_query) && !empty($varname_query) )
-					$or = ' OR ';
-				$extra_param = " AND ( $varname_query $or $text_query )";
-			}
-		}
+    if(isset($_GET['search_phrase']))
+    {
+        $varname = mysql_clean($_GET['varname']);
+        $text = mysql_clean($_GET['text']);
 
-		$lang_phrases = $lang_obj->get_phrases($edit_id,'*',$curr_limit,$extra_param);
-		$total_phrases = $lang_obj->count_phrases($edit_id,$extra_param);
+        if(!empty($varname)){
+            $varname_query = "varname LIKE '%$varname%'";
+        }
+        if(!empty($text)){
+            $text_query = "text LIKE '%$text%'";
+        }
 
-		assign('lang_phrases',$lang_phrases);
+        if(!empty($text_query) || !empty($varname_query)) {
+            if(!empty($text_query) && !empty($varname_query) ){
+                $or = ' OR ';
+            }
+            $extra_param = " AND ( $varname_query $or $text_query )";
+        }
+    }
 
-		//Collecting Data for Pagination
-		$total_pages = $total_phrases/$limit;
-		$total_pages = round($total_pages+0.49,0);
-		//Pagination
-		$pages->paginate($total_pages-2,$current_page);
-	}
+    $lang_phrases = $lang_obj->get_phrases($edit_id,'*',$curr_limit,$extra_param);
+    $total_phrases = $lang_obj->count_phrases($edit_id,$extra_param);
 
-	assign('client_id',$Cbucket->configs['clientid']);
-	assign('secret_Id',$Cbucket->configs['secretId']);
+    assign('lang_phrases',$lang_phrases);
 
-	subtitle("Language Settings");
-	template_files('language_settings.html');
-	display_it();
+    //Collecting Data for Pagination
+    $total_pages = $total_phrases/$limit;
+    $total_pages = round($total_pages+0.49,0);
+    //Pagination
+    $pages->paginate($total_pages-2,$current_page);
+}
+
+assign('client_id',$Cbucket->configs['clientid']);
+assign('secret_Id',$Cbucket->configs['secretId']);
+
+subtitle('Language Settings');
+template_files('language_settings.html');
+display_it();
