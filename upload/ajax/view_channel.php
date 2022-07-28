c3cd53c53eb69819b604cb1beab6da6860b30746
MacWarrior <macwarrior94@gmail.com>
2022-07-28 23:42:55 +0200

    Front office : Fix channel load more videos

--- upload/ajax/view_channel.php
+++ upload/ajax/view_channel.php
@@ -1,43 +1,45 @@
 <?php
 define('THIS_PAGE','ajax');
 require '../includes/config.inc.php';
+
 if (isset($_POST['mode'])) {
     $mode = $_POST['mode'];
+
     switch ($mode) {
         case 'channelMore':
-            global $db, $cbvid, $Smarty;
             $load_hit = $_POST['loadHit'];
             $load_limit = $_POST['loadLimit'];
             $user = $_POST['userid'];
             $start = $load_limit * $load_hit - $load_limit;
             $sql_limit = "$start, $load_limit";
             $total_items = $_POST['totalVids'];
-            $items = get_videos(array("user"=>$user, "order"=>"date_added DESC","limit"=>$sql_limit));
+            $items = get_videos(['user'=>$user, 'order'=>'date_added DESC','limit'=>$sql_limit]);
             if ($start >= $total_items) {
                 return false;
             }
+
             foreach ($items as $key => $video) {
-                assign("video", $video);
-                assign("display_type","ajaxHome");
-                echo trim(Fetch('/blocks//videos/video.html'));
+                assign('video', $video);
+                assign('display_type','ajaxHome');
+                get_fast_qlist();
+                echo trim(Fetch('blocks/videos/video.html'));
             }
             break;
 
         case 'channelMorePhotos':
-            global $db, $cbvid, $Smarty;
             $load_hit = $_POST['loadHit'];
             $load_limit = $_POST['loadLimit'];
             $user = $_POST['user'];
             $start = $load_limit * $load_hit - $load_limit;
             $sql_limit = "$start, $load_limit";
             $total_items = $_POST['totalPhotos'];
-            $items = get_photos(array("user"=>$user, "order"=>"date_added DESC","limit"=>$sql_limit));
+            $items = get_photos(['user'=>$user, 'order'=>'date_added DESC','limit'=>$sql_limit]);
             if ($start >= $total_items) {
                 return false;
             }
             foreach ($items as $key => $p_list) {
-                assign("photo", $p_list);
-                assign("display_type","view_channelAjax");
+                assign('photo', $p_list);
+                assign('display_type','view_channelAjax');
                 echo trim(Fetch('/blocks/photo.html'));
             }
             break;
