c5d548203efb90c0cb70f895cfcdd34cbfa541d5
clement-sanz <clement@oxygenz.fr>
2023-09-21 08:47:49 +0200

    commit temp

---
+++ upload/includes/classes/tmdb.class.php
@@ -0,0 +1,42 @@
+<?php
+
+class Tmdb
+{
+
+    const API_URL = 'https://api.themoviedb.org/3/';
+    private static $curl;
+
+    /**
+     * @param \Classes\Curl $curl
+     * @return void
+     */
+    public static function init(\Classes\Curl $curl)
+    {
+
+    }
+
+    public static function searchMovie($search)
+    {
+
+    }
+
+    public static function movieDetail($movie_id)
+    {
+
+    }
+
+    public static function movieCredits($movie_id)
+    {
+
+    }
+
+    public static function setLanguage($language)
+    {
+
+    }
+
+    public static function personDetail($person_id)
+    {
+
+    }
+}
