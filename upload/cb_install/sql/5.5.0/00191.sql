93670cdb37a4db99d3457ba1294618dadf8f0968
MacWarrior <macwarrior94@gmail.com>
2023-11-06 15:54:19 +0100

    Miscellaneous : Update PHP 8+ compatibility, Cleanup code, fix SQL update

--- upload/cb_install/sql/5.5.0/00191.sql
+++ upload/cb_install/sql/5.5.0/00191.sql
@@ -25,7 +25,7 @@ INSERT IGNORE INTO `{tbl_prefix}languages_translations` (`id_language_key`, `tra
 INSERT IGNORE INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
     VALUES (@id_language_key, 'Gestion des fichiers de sous-titre', @language_id_fra);
 
-SET @language_key = 'video_subtitle_management' COLLATE utf8mb4_unicode_520_ci;
+SET @language_key = 'video_subtitles_deleted_num' COLLATE utf8mb4_unicode_520_ci;
 INSERT IGNORE INTO `{tbl_prefix}languages_keys` (`language_key`) VALUES (@language_key);
 SET @id_language_key = (SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` COLLATE utf8mb4_unicode_520_ci = @language_key);
 INSERT IGNORE INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
--- upload/cb_install/sql/5.5.0/00270.sql
+++ upload/cb_install/sql/5.5.0/00270.sql
@@ -21,4 +21,3 @@ DELETE FROM `{tbl_prefix}languages_keys`WHERE `id_language_key` = @id_language_k
 SET @id_language_key = (SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'user_rate_comment');
 DELETE FROM `{tbl_prefix}languages_translations` WHERE `id_language_key` = @id_language_key;
 DELETE FROM `{tbl_prefix}languages_keys`WHERE `id_language_key` = @id_language_key;
-
