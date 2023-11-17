375631cdbe411ec478d4e40fbc473865fb65f832
clement-sanz <clement@oxygenz.fr>
2023-11-17 16:24:02 +0100

    correction control module version for install

--- upload/cb_install/modes/precheck.php
+++ upload/cb_install/modes/precheck.php
@@ -6,27 +6,80 @@
 </div>
 
 <div id="sub_container">
-    <dl>
-        <dt class="grey-text">PHP CLI</dt>
-        <?php $php_ver = check_module('php'); ?>
-        <dd><span style="margin-left:60px;" class="grey-text"><?php echo msg_arr($php_ver); ?></span></dd>
-
-        <dt class="grey-text">FFMPEG</dt>
-        <?php $ffmpeg_ver = check_module('ffmpeg'); ?>
-        <dd style="background-color:#fff;"><span style="margin-left:60px;" class="grey-text"><?php echo msg_arr($ffmpeg_ver); ?></span></dd>
-
-        <dt class="grey-text">FFPROBE</dt>
-        <?php $ffprobe_ver = check_module('ffprobe'); ?>
-        <dd><span style="margin-left:60px;" class="grey-text"><?php echo msg_arr($ffprobe_ver); ?></span></dd>
-
-        <dt class="grey-text">cURL</dt>
-        <?php $curlver = check_module('curl'); ?>
-        <dd><span style="margin-left:60px;" class="grey-text"><?php echo msg_arr($curlver); ?></span></dd>
-
-        <dt class="grey-text">Media Info</dt>
-        <?php $media_info = check_module('media_info'); ?>
-        <dd style="background-color:#fff;"><span style="margin-left:60px;" class="grey-text"><?php echo msg_arr($media_info); ?></span></dd>
-    </dl>
+    <table class="grey-text">
+        <tbody>
+            <tr>
+                <th style="min-width: 80px;">PHP Web</th>
+                <td></td>
+                <td><?php echo msg_arr(check_module('php_web')); ?></td>
+            </tr>
+            <tr>
+                <td rowspan="5"></td>
+                <th style="min-width: 95px;">GD</th>
+                <td><?php echo msg_arr(check_extension('gd', 'web')); ?></td>
+            </tr>
+            <tr>
+                <th style="min-width: 95px;">MBstring</th>
+                <td><?php echo msg_arr(check_extension('mbstring', 'web')); ?></td>
+            </tr>
+            <tr>
+                <th style="min-width: 95px;">MySQLi</th>
+                <td><?php echo msg_arr(check_extension('mysqli', 'web')); ?></td>
+            </tr>
+            <tr>
+                <th style="min-width: 95px;">XML</th>
+                <td><?php echo msg_arr(check_extension('xml', 'web')); ?></td>
+            </tr>
+            <tr>
+                <th style="min-width: 95px;">cURL</th>
+                <td><?php echo msg_arr(check_extension('curl', 'web')); ?></td>
+            </tr>
+        <tbody>
+            <tr>
+                <th style="min-width: 80px;">PHP CLI</th>
+                <td></td>
+                <td><?php echo msg_arr(check_module('php_cli')); ?></td>
+            </tr>
+            <tr>
+                <td rowspan="5"></td>
+                <th style="min-width: 95px;">GD</th>
+                <td><?php echo msg_arr(check_extension('gd', 'cli')); ?></td>
+            </tr>
+            <tr>
+                <th style="min-width: 95px;">MBstring</th>
+                <td><?php echo msg_arr(check_extension('mbstring', 'cli')); ?></td>
+            </tr>
+            <tr>
+                <th style="min-width: 95px;">MySQLi</th>
+                <td><?php echo msg_arr(check_extension('mysqli', 'cli')); ?></td>
+            </tr>
+            <tr>
+                <th style="min-width: 95px;">XML</th>
+                <td><?php echo msg_arr(check_extension('xml', 'cli')); ?></td>
+            </tr>
+            <tr>
+                <th style="min-width: 95px;">cURL</th>
+                <td><?php echo msg_arr(check_extension('curl', 'cli')); ?></td>
+            </tr>
+        </tbody>
+        <tbody>
+        <tr>
+            <th style="min-width: 80px;">FFMPEG</th>
+            <td></td>
+            <td><?php echo msg_arr(check_module('ffmpeg')); ?></td>
+        </tr>
+        <tr>
+            <th style="min-width: 80px;">FFPROBE</th>
+            <td></td>
+            <td><?php echo msg_arr(check_module('ffprobe')); ?></td>
+        </tr>
+        <tr>
+            <th style="min-width: 80px;">Media Info</th>
+            <td></td>
+            <td><?php echo msg_arr(check_module('media_info')); ?></td>
+        </tr>
+        </tbody>
+    </table>
 
     <form method="post" id="installation">
         <input type="hidden" name="mode" value="permission"/>
