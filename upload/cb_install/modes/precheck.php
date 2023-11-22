ee45926436df816438252c0e304e20670bad571d
MacWarrior <macwarrior94@gmail.com>
2023-11-22 16:20:42 +0100

    Fix CSS, remove unused files

--- upload/cb_install/modes/precheck.php
+++ upload/cb_install/modes/precheck.php
@@ -9,35 +9,35 @@
     <dl>
         <dt>PHP Web</dt>
         <dd><span><?php echo msg_arr(check_module('php_web')); ?></span></dd>
-        <dt><span>GD</dt>
-        <dd><span><?php echo msg_arr(check_extension('gd', 'web')); ?></span></dd>
+        <dt class="white"><span>GD</dt>
+        <dd class="white"><span><?php echo msg_arr(check_extension('gd', 'web')); ?></span></dd>
         <dt><span>MBstring</dt>
         <dd><span><?php echo msg_arr(check_extension('mbstring', 'web')); ?></span></dd>
-        <dt><span>MySQLi</dt>
-        <dd><span><?php echo msg_arr(check_extension('mysqli', 'web')); ?></span></dd>
+        <dt class="white"><span>MySQLi</dt>
+        <dd class="white"><span><?php echo msg_arr(check_extension('mysqli', 'web')); ?></span></dd>
         <dt><span>XML</dt>
         <dd><span><?php echo msg_arr(check_extension('xml', 'web')); ?></span></dd>
-        <dt><span>cURL</dt>
-        <dd><span><?php echo msg_arr(check_extension('curl', 'web')); ?></span></dd>
+        <dt class="white"><span>cURL</dt>
+        <dd class="white"><span><?php echo msg_arr(check_extension('curl', 'web')); ?></span></dd>
 
         <dt>PHP CLI</dt>
         <dd><span><?php echo msg_arr(check_module('php_cli')); ?></span></dd>
-        <dt><span>GD</dt>
-        <dd><span><?php echo msg_arr(check_extension('gd', 'cli')); ?></span></dd>
+        <dt class="white"><span>GD</dt>
+        <dd class="white"><span><?php echo msg_arr(check_extension('gd', 'cli')); ?></span></dd>
         <dt><span>MBstring</dt>
         <dd><span><?php echo msg_arr(check_extension('mbstring', 'cli')); ?></span></dd>
-        <dt><span>MySQLi</dt>
-        <dd><span><?php echo msg_arr(check_extension('mysqli', 'cli')); ?></span></dd>
+        <dt class="white"><span>MySQLi</dt>
+        <dd class="white"><span><?php echo msg_arr(check_extension('mysqli', 'cli')); ?></span></dd>
         <dt><span>XML</dt>
         <dd><span><?php echo msg_arr(check_extension('xml', 'cli')); ?></span></dd>
-        <dt><span>cURL</dt>
-        <dd><span><?php echo msg_arr(check_extension('curl', 'cli')); ?></span></dd>
+        <dt class="white"><span>cURL</dt>
+        <dd class="white"><span><?php echo msg_arr(check_extension('curl', 'cli')); ?></span></dd>
 
         <dt>FFMPEG</dt>
         <dd><span><?php echo msg_arr(check_module('ffmpeg')); ?></span></dd>
 
-        <dt>FFPROBE</dt>
-        <dd><span><?php echo msg_arr(check_module('ffprobe')); ?></span></dd>
+        <dt class="white">FFPROBE</dt>
+        <dd class="white"><span><?php echo msg_arr(check_module('ffprobe')); ?></span></dd>
 
         <dt>Media Info</dt>
         <dd><span><?php echo msg_arr(check_module('media_info')); ?></span></dd>
