d3ba78a572d06c4a5f937607812af35843dee628
MacWarrior <macwarrior94@gmail.com>
2024-01-16 17:44:53 +0100

    Cleanup

--- upload/includes/functions.php
+++ upload/includes/functions.php
@@ -4198,14 +4198,20 @@ function find_string($needle_start, $needle_end, $results)
 	* Checks : MEMORY_LIMIT, UPLOAD_MAX_FILESIZE, POST_MAX_SIZE, MAX_EXECUTION_TIME
 	* If any of these configs are less than required value, warning is shown
     */
+/**
+ * @throws Exception
+ */
 function check_server_confs()
 {
-    define('POST_MAX_SIZE', ini_get('post_max_size'));
-    define('MEMORY_LIMIT', ini_get('memory_limit'));
-    define('UPLOAD_MAX_FILESIZE', ini_get('upload_max_filesize'));
-    define('MAX_EXECUTION_TIME', ini_get('max_execution_time'));
+    $post_max_size = ini_get('post_max_size');
+    $memory_limit = ini_get('memory_limit');
+    $upload_max_filesize = ini_get('upload_max_filesize');
+    $max_execution_time = ini_get('max_execution_time');
 
-    if (getBytesFromFileSize(POST_MAX_SIZE) < getBytesFromFileSize('50M') || getBytesFromFileSize(MEMORY_LIMIT) < getBytesFromFileSize('128M') || getBytesFromFileSize(UPLOAD_MAX_FILESIZE) < getBytesFromFileSize('50M') && MAX_EXECUTION_TIME < 7200) {
+    if (getBytesFromFileSize($post_max_size) < getBytesFromFileSize('50M')
+        || getBytesFromFileSize($memory_limit) < getBytesFromFileSize('128M')
+        || getBytesFromFileSize($upload_max_filesize) < getBytesFromFileSize('50M')
+        || $max_execution_time < 7200) {
         e(sprintf(lang('error_server_config'), '/admin_area/system_info.php#hosting'), 'w', false);
     }
 }
