0f26ca25c45390fb1f4aea1cfbe14f1d0e923f64
clement-sanz <clement@oxygenz.fr>
2024-02-09 18:20:48 +0100

    correct readonly tags

--- upload/styles/cb_28/theme/js/init_readonly_tag/init_readonly_tag.js
+++ upload/styles/cb_28/theme/js/init_readonly_tag/init_readonly_tag.js
@@ -1,17 +1,23 @@
-$(function () {
-    $('#list_tags').tagit({
-        singleField:true,
-        fieldName:"tags",
-        readOnly:true,
-        singleFieldNode:$('#tags'),
-        animate:false,
-        caseSensitive:false
-    });
-
-    if (link_type !== undefined && link_type) {
-        var labels = $('.tagit-label');
-        labels.each(function () {
-            $(this).parent().wrap('<a href="/search_result.php?query=' + $(this).html()+ '&type=' + link_type + '"></a>');
+    function init_readonly_tags(id_input, list_tag) {
+        if (typeof list_tag == 'undefined') {
+            list_tag = '#list_tags';
+        }
+        if (typeof id_input == 'undefined') {
+            id_input = 'tags';
+        }
+        $(list_tag).tagit({
+            singleField: true,
+            fieldName: "tags",
+            readOnly: true,
+            singleFieldNode: $('#' + id_input),
+            animate: false,
+            caseSensitive: false
         });
+
+        if (link_type !== undefined && link_type) {
+            var labels = $('.tagit-label');
+            labels.each(function () {
+                $(this).parent().wrap('<a href="/search_result.php?query=' + $(this).html() + '&type=' + link_type + '"></a>');
+            });
+        }
     }
-})
