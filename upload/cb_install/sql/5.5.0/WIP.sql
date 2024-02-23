fba1b6da6abd736fbf306d6a9dea1c1c06e29b2b
MacWarrior <macwarrior94@gmail.com>
2024-02-23 11:20:08 +0100

    Fix SQL

--- upload/cb_install/sql/5.5.0/WIP.sql
+++ upload/cb_install/sql/5.5.0/WIP.sql
@@ -70,14 +70,6 @@ VALUES (@id_language_key, 'Release date', @language_id_eng);
 INSERT IGNORE INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
 VALUES (@id_language_key, 'Date de sortie', @language_id_fra);
 
-SET @language_key = 'poster' COLLATE utf8mb4_unicode_520_ci;
-INSERT IGNORE INTO `{tbl_prefix}languages_keys` (`language_key`) VALUES (@language_key);
-SET @id_language_key = (SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` COLLATE utf8mb4_unicode_520_ci = @language_key);
-INSERT IGNORE INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
-VALUES (@id_language_key, 'Poster', @language_id_eng);
-INSERT IGNORE INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
-VALUES (@id_language_key, 'Affiche', @language_id_fra);
-
 SET @language_key = 'import' COLLATE utf8mb4_unicode_520_ci;
 INSERT IGNORE INTO `{tbl_prefix}languages_keys` (`language_key`) VALUES (@language_key);
 SET @id_language_key = (SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` COLLATE utf8mb4_unicode_520_ci = @language_key);
