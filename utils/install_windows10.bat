e067c582d673ed84f38dcd6382a9d34a99578e1f
MacWarrior <macwarrior94@gmail.com>
2024-04-22 15:33:32 +0200

    Update GIT, MariaDB, Nginx, PHP, FFmpeg, MediaInfo

--- utils/install_windows10.bat
+++ utils/install_windows10.bat
@@ -40,8 +40,8 @@ echo |set /p=Creating GIT directory...
 SET "GIT_DIR=%CB_DIR%\git"
 md %GIT_DIR%
 echo OK
-echo |set /p=Downloading GIT 2.43.0 install...
-SET "GIT_URL=https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/PortableGit-2.43.0-64-bit.7z.exe"
+echo |set /p=Downloading GIT 2.45.0 install...
+SET "GIT_URL=https://github.com/git-for-windows/git/releases/download/v2.45.0-rc0.windows.1/PortableGit-2.45.0-rc0-64-bit.7z.exe"
 SET "GIT_EXE_FILENAME=install_git.exe"
 SET "GIT_EXE=%GIT_DIR%\%GIT_EXE_FILENAME%"
 Powershell.exe -command "(New-Object System.Net.WebClient).DownloadFile('%GIT_URL%','%GIT_EXE%')"
@@ -62,9 +62,9 @@ echo |set /p=Creating MariaDB directory...
 SET "MARIADB_DIR=%CB_DIR%\mariadb"
 md %MARIADB_DIR%
 echo OK
-echo |set /p=Downloading MariaDB 11.2.2 install...
-SET "MARIADB_URL=https://mirrors.ircam.fr/pub/mariadb/mariadb-11.2.2/winx64-packages/mariadb-11.2.2-winx64.zip"
-SET "MARIADB_ZIP_FILENAME=mariadb-11.2.2.zip"
+echo |set /p=Downloading MariaDB 11.5.0 install...
+SET "MARIADB_URL=https://mirrors.ircam.fr/pub/mariadb/mariadb-11.5.0/winx64-packages/mariadb-11.5.0-winx64.zip"
+SET "MARIADB_ZIP_FILENAME=mariadb-11.5.0.zip"
 SET "MARIADB_ZIP=%MARIADB_DIR%\%MARIADB_ZIP_FILENAME%"
 Powershell.exe -command "(New-Object System.Net.WebClient).DownloadFile('%MARIADB_URL%','%MARIADB_ZIP%')"
 echo OK
@@ -80,16 +80,16 @@ echo |set /p=Creating Nginx directory...
 SET "NGINX_DIR=%CB_DIR%\nginx"
 md %NGINX_DIR%
 echo OK
-echo |set /p=Downloading Nginx 1.25.3 install...
-SET "NGINX_URL=https://nginx.org/download/nginx-1.25.3.zip"
-SET "NGINX_ZIP_FILENAME=nginx-1.25.3.zip"
+echo |set /p=Downloading Nginx 1.25.5 install...
+SET "NGINX_URL=https://nginx.org/download/nginx-1.25.5.zip"
+SET "NGINX_ZIP_FILENAME=nginx-1.25.5.zip"
 SET "NGINX_ZIP=%NGINX_DIR%\%NGINX_ZIP_FILENAME%"
 Powershell.exe -command "(New-Object System.Net.WebClient).DownloadFile('%NGINX_URL%','%NGINX_ZIP%')"
 echo OK
 echo |set /p=Extracting Nginx...
 Powershell.exe -command "$ProgressPreference = 'SilentlyContinue'; Expand-Archive -Force '%NGINX_ZIP%' '%NGINX_DIR%'"
-Powershell.exe -command "Move-Item %NGINX_DIR%\nginx-1.25.3*\* %NGINX_DIR%"
-Powershell.exe -command "Remove-Item %NGINX_DIR%\nginx-1.25.3*"
+Powershell.exe -command "Move-Item %NGINX_DIR%\nginx-1.25.5*\* %NGINX_DIR%"
+Powershell.exe -command "Remove-Item %NGINX_DIR%\nginx-1.25.5*"
 echo OK
 
 :install_php
@@ -98,9 +98,9 @@ echo |set /p=Creating PHP directory...
 SET "PHP_DIR=%CB_DIR%\php"
 md %PHP_DIR%
 echo OK
-echo |set /p=Downloading PHP 8.3.1 install...
-SET "PHP_URL=https://windows.php.net/downloads/releases/php-8.3.1-Win32-vs16-x64.zip"
-SET "PHP_ZIP_FILENAME=php-8.3.1.zip"
+echo |set /p=Downloading PHP 8.3.6 install...
+SET "PHP_URL=https://windows.php.net/downloads/releases/php-8.3.6-Win32-vs16-x64.zip"
+SET "PHP_ZIP_FILENAME=php-8.3.6.zip"
 SET "PHP_ZIP=%PHP_DIR%\%PHP_ZIP_FILENAME%"
 :: We're faking Edge useragent to prevent PHP website to block download
 Powershell.exe -command "$cli = New-Object System.Net.WebClient; $cli.Headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.2210.91'; $cli.DownloadFile('%PHP_URL%', '%PHP_ZIP%')"
@@ -117,16 +117,16 @@ echo |set /p=Creating FFMpeg directory...
 SET "FFMPEG_DIR=%CB_DIR%\ffmpeg"
 md %FFMPEG_DIR%
 echo OK
-echo |set /p=Downloading FFMpeg 6.1.1 install...
-SET "FFMPEG_URL=https://github.com/GyanD/codexffmpeg/releases/download/6.1.1/ffmpeg-6.1.1-full_build.zip"
-SET "FFMPEG_ZIP_FILENAME=ffmpeg-6.1.1.zip"
+echo |set /p=Downloading FFMpeg 7.0 install...
+SET "FFMPEG_URL=https://github.com/GyanD/codexffmpeg/releases/download/7.0/ffmpeg-7.0-full_build.zip"
+SET "FFMPEG_ZIP_FILENAME=ffmpeg-7.0.zip"
 SET "FFMPEG_ZIP=%FFMPEG_DIR%\%FFMPEG_ZIP_FILENAME%"
 Powershell.exe -command "(New-Object System.Net.WebClient).DownloadFile('%FFMPEG_URL%','%FFMPEG_ZIP%')"
 echo OK
 echo |set /p=Extracting FFMpeg...
 Powershell.exe -command "$ProgressPreference = 'SilentlyContinue'; Expand-Archive -Force '%FFMPEG_ZIP%' '%FFMPEG_DIR%'"
-Powershell.exe -command "Move-Item %FFMPEG_DIR%\ffmpeg-6.1.1*\* %FFMPEG_DIR%"
-Powershell.exe -command "Remove-Item %FFMPEG_DIR%\ffmpeg-6.1.1*"
+Powershell.exe -command "Move-Item %FFMPEG_DIR%\ffmpeg-7.0*\* %FFMPEG_DIR%"
+Powershell.exe -command "Remove-Item %FFMPEG_DIR%\ffmpeg-7.0*"
 echo OK
 
 :install_mediainfo
@@ -135,9 +135,9 @@ echo |set /p=Creating MediaInfo directory...
 SET "MEDIAINFO_DIR=%CB_DIR%\mediainfo"
 md %MEDIAINFO_DIR%
 echo OK
-echo |set /p=Downloading MediaInfo 23.11.1 install...
-SET "MEDIAINFO_URL=https://mediaarea.net/download/binary/mediainfo/23.11.1/MediaInfo_CLI_23.11.1_Windows_x64.zip"
-SET "MEDIAINFO_ZIP_FILENAME=mediainfo-23.11.1.zip"
+echo |set /p=Downloading MediaInfo 24.04 install...
+SET "MEDIAINFO_URL=https://mediaarea.net/download/binary/mediainfo/24.04/MediaInfo_CLI_24.04_Windows_x64.zip"
+SET "MEDIAINFO_ZIP_FILENAME=mediainfo-24.04.zip"
 SET "MEDIAINFO_ZIP=%MEDIAINFO_DIR%\%MEDIAINFO_ZIP_FILENAME%"
 Powershell.exe -command "(New-Object System.Net.WebClient).DownloadFile('%MEDIAINFO_URL%','%MEDIAINFO_ZIP%')"
 echo OK
