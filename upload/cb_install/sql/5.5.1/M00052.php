17055c6ec9176a47ce74ce82bc40171758e7122e
MacWarrior <macwarrior94@gmail.com>
2024-07-05 15:21:08 +0200

    Update revision & changelog, cleanup

--- upload/cb_install/sql/5.5.1/M00052.php
+++ upload/cb_install/sql/5.5.1/M00052.php
@@ -2,13 +2,13 @@
 namespace V5_5_1;
 require_once \DirPath::get('classes') . DIRECTORY_SEPARATOR . 'migration' . DIRECTORY_SEPARATOR . 'migration.class.php';
 
-class WIP extends \Migration
+class M00052 extends \Migration
 {
     /**
      * @throws \Exception
      */
     public function start()
     {
-        self::query('insert into `{tbl_prefix}config` (`name`, `value`) VALUES (\'enable_edit_photo_button\', \'yes\')');
+        self::generateConfig('enable_edit_photo_button', 'yes');
     }
 }
