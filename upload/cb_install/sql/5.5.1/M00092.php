c4e99e5bad60966a31dff0d968d826772861e6b8
MacWarrior <macwarrior94@gmail.com>
2024-08-07 19:14:28 +0200

    Cleanup, fix revision & changelog

---
+++ upload/cb_install/sql/5.5.1/M00092.php
@@ -0,0 +1,22 @@
+<?php
+namespace V5_5_1;
+require_once \DirPath::get('classes') . DIRECTORY_SEPARATOR . 'migration' . DIRECTORY_SEPARATOR . 'migration.class.php';
+
+class M00092 extends \Migration
+{
+    /**
+     * @throws \Exception
+     */
+    public function start()
+    {
+        self::generateTranslation('thumb_regen_start', [
+            'fr'=>'Renénération des vignettes en cours...',
+            'en'=>'Ongoing thumbs regeneration...'
+        ]);
+
+        self::generateTranslation('thumb_regen_end', [
+            'fr'=>'Regénération des vignette terminée',
+            'en'=> 'Thumbs regeneration ended'
+        ]);
+    }
+}
