f4695614a7b21b56df2d3009ee471fc2c2b33bb3
nvanhaezebrouck <nicolas@oxygenz.fr>
2024-08-09 15:59:06 +0200

    add log on isDateTimeSynchro

--- upload/includes/classes/system.class.php
+++ upload/includes/classes/system.class.php
@@ -720,6 +720,7 @@ class System{
 
         $details['bdd'] = $rs[0]['t'];
         $details['php'] = date('Y-m-d H:i:s');
+        $details['php_timezone_default'] = date_default_timezone_get();
 
         $datetime1 = new \DateTime($details['bdd']);
         $datetime2 = new \DateTime($details['php']);
