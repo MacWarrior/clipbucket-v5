4741d8244a970130d25569e217fd59528497cd6c
MacWarrior <macwarrior94@gmail.com>
2024-09-11 08:59:25 +0200

    Update revision & changelog

--- upload/cb_install/sql/5.5.1/M00128.php
+++ upload/cb_install/sql/5.5.1/M00128.php
@@ -3,7 +3,7 @@
 namespace V5_5_1;
 require_once \DirPath::get('classes') . DIRECTORY_SEPARATOR . 'migration' . DIRECTORY_SEPARATOR . 'migration.class.php';
 
-class MWIP extends \Migration
+class M00128 extends \Migration
 {
     /**
      * @throws \Exception
@@ -12,7 +12,7 @@ class MWIP extends \Migration
     {
         self::generateTranslation('collection_not_found', [
             'fr' => 'Une collection est nécessaire pour compléter la configuration de la photo',
-            'en'=>'A collection is requiered to complete photo configuration'
+            'en' => 'A collection is requiered to complete photo configuration'
         ]);
 
         self::alterTable('ALTER TABLE ' . tbl('photos') . ' DROP COLUMN collection_id', [
