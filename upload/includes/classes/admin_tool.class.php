0f811c0ccf540bc3944c4a7a25b59d9216451b6e
csanz <clement@oxygenz.fr>
2024-09-11 11:36:18 +0200

    Back office : Fix user storage running even when disabled
    ---------
    Co-authored-by: MacWarrior <macwarrior94@gmail.com>

--- upload/includes/classes/admin_tool.class.php
+++ upload/includes/classes/admin_tool.class.php
@@ -743,11 +743,13 @@ class AdminTool
 
     public function calcUserStorage()
     {
-        $users = User::getInstance()->getAll([
-            'condition'=>' users.userid != ' . mysql_clean(userquery::getInstance()->get_anonymous_user()) . ' AND usr_status like \'ok\''
-        ]) ?: [];
-        $this->array_loop = array_column($users, 'userid') ;
-        $this->executeTool('User::calcUserStorage');
+        if (config('enable_storage_history') == 'yes') {
+            $users = User::getInstance()->getAll([
+                'condition'=>' users.userid != ' . mysql_clean(userquery::getInstance()->get_anonymous_user()) . ' AND usr_status like \'ok\''
+            ]) ?: [];
+            $this->array_loop = array_column($users, 'userid') ;
+            $this->executeTool('User::calcUserStorage');
+        }
     }
 
     /**
