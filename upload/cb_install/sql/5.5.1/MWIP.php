240de97204cb864e1e389469ce56b235a40c40a4
clement-sanz <clement@oxygenz.fr>
2024-09-23 11:16:04 +0200

    fix default permissions

--- upload/cb_install/sql/5.5.1/MWIP.php
+++ upload/cb_install/sql/5.5.1/MWIP.php
@@ -18,10 +18,17 @@ class MWIP extends \Migration
             ]
         );
 
+        $sql_set = 'set @user_level_id = (SELECT user_level_id FROM ' . tbl('user_levels') . ' WHERE user_level_name = \'Guest\') ';
+        self::query($sql_set);
+        $sql = ' UPDATE ' . tbl('user_levels_permissions') .' SET `allow_photo_upload` = \'no\' WHERE user_level_id = @user_level_id';
+        self::query($sql);
         self::generateTranslation('allow_photo_upload', [
             'fr'=>'Autorise le téléversement de photo',
             'en'=>'Allow photo upload'
         ]);
+
+        self::query('UPDATE ' . tbl('user_levels') . ' SET user_level_is_default = \'no\' ');
+        self::query('UPDATE ' . tbl('user_levels') . ' SET user_level_is_default = \'yes\' WHERE user_level_id = 2 ');
 
         $sql = 'INSERT IGNORE INTO `{tbl_prefix}user_permissions` (`permission_type`, `permission_name`, `permission_code`, `permission_desc`, `permission_default`) 
             VALUES ( 2, \'Allow Photo Upload\', \'allow_photo_upload\', \'Allow user to upload photos\', \'yes\')';
