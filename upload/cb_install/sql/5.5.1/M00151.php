14f43fdc31d6028750df52323d56253ad1ed50ab
MacWarrior <macwarrior94@gmail.com>
2024-09-30 13:03:43 +0200

    Miscellaneous : Add index to sessions table

---
+++ upload/cb_install/sql/5.5.1/M00151.php
@@ -0,0 +1,17 @@
+<?php
+namespace V5_5_1;
+require_once \DirPath::get('classes') . DIRECTORY_SEPARATOR . 'migration' . DIRECTORY_SEPARATOR . 'migration.class.php';
+
+class M00151 extends \Migration
+{
+    /**
+     * @throws \Exception
+     */
+    public function start()
+    {
+        self::alterTable('ALTER TABLE ' . tbl('sessions') . ' ADD INDEX(`session_date`);', [
+            'table'  => 'sessions',
+            'column' => 'session_date'
+        ]);
+    }
+}
--- upload/cb_install/sql/structure.sql
+++ upload/cb_install/sql/structure.sql
@@ -1178,3 +1178,6 @@ ALTER TABLE `{tbl_prefix}social_networks_links`
     MODIFY `id_social_networks_link` int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `{tbl_prefix}social_networks_links`
     ADD CONSTRAINT `social_networks_links_ibfk_1` FOREIGN KEY (`id_fontawesome_icon`) REFERENCES `{tbl_prefix}fontawesome_icons` (`id_fontawesome_icon`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `{tbl_prefix}sessions`
+    ADD INDEX(`session_date`);
