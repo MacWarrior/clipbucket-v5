cb124efc9acf4f4f8ec6cca9d2b0d1801289fece
csanz <clement@oxygenz.fr>
2024-10-09 16:38:07 +0200

    Back office : Update user tags order & display
    ---------
    Co-authored-by: MacWarrior <macwarrior94@gmail.com>

--- upload/cb_install/sql/language_FRA.sql
+++ upload/cb_install/sql/language_FRA.sql
@@ -1775,3 +1775,13 @@ INSERT INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translatio
 VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'force_to_error'), 'Forcer en erreur', @language_id);
 INSERT INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
 VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'tool_forced_to_error'), 'Outil forcé en erreur', @language_id);
+INSERT INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
+VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'user_level'), 'Niveau d''utilisateur', @language_id);
+INSERT INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
+VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'administrator'), 'Administrateur', @language_id);
+INSERT INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
+VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'registered_user'), 'Utilisateur enregistré', @language_id);
+INSERT INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
+VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'inactive_user'), 'Utilisateur inactif', @language_id);
+INSERT INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
+VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'global_moderator'), 'Modérateur global', @language_id);
--- upload/cb_install/sql/languages.sql
+++ upload/cb_install/sql/languages.sql
@@ -1290,4 +1290,9 @@ VALUES ('admin_setting'),
        ('writeable'),
        ('chmod_file'),
        ('force_to_error'),
-       ('tool_forced_to_error');
+       ('tool_forced_to_error'),
+       ('user_level'),
+       ('administrator'),
+       ('registered_user'),
+       ('inactive_user'),
+       ('global_moderator');
