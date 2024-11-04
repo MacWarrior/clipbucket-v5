c69388b67a88b82656f1bc0150b4ff5ad936afcd
clement-sanz <clement@oxygenz.fr>
2024-11-04 17:08:08 +0100

    336 remove debug sleep

--- upload/admin_area/sse/update_info.php
+++ upload/admin_area/sse/update_info.php
@@ -3,7 +3,6 @@ const THIS_PAGE = 'update_info';
 require_once dirname(__FILE__, 3) . '/includes/admin_config.php';
 require_once DirPath::get('classes') . 'SSE.class.php';
 userquery::getInstance()->admin_login_check();
-sleep(3);
 SSE::processSSE(function () {
     if (Update::IsCurrentDBVersionIsHigherOrEqualTo(AdminTool::MIN_VERSION_CODE, AdminTool::MIN_REVISION_CODE)) {
         $column = 'code';
