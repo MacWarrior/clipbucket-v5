b948761d50707fd9585316261e28152c92003936
MacWarrior <macwarrior94@gmail.com>
2024-12-04 20:18:54 +0100

    Fix migration 5.5.1 - 203

--- upload/cb_install/sql/5.5.1/M00203.php
+++ upload/cb_install/sql/5.5.1/M00203.php
@@ -10,8 +10,8 @@ class M00203 extends \Migration
      */
     public function start()
     {
-        $sql = 'INSERT INTO `{tbl_prefix}user_profile` (userid) 
-                    SELECT DISTINCT `{tbl_prefix}users`.`userid` 
+        $sql = 'INSERT INTO `{tbl_prefix}user_profile` (`userid`, `profile_title`, `profile_desc`, `featured_video`, `about_me`, `schools`, `occupation`, `companies`, `hobbies`, `fav_movies`, `fav_music`, `fav_books`, `background`, `voters`) 
+                    SELECT DISTINCT `{tbl_prefix}users`.`userid`, \'\', \'\', \'\', \'\', \'\', \'\', \'\', \'\', \'\', \'\', \'\', \'\'
                     FROM `{tbl_prefix}users` 
                     LEFT JOIN `{tbl_prefix}user_profile` ON `{tbl_prefix}users`.`userid` = `{tbl_prefix}user_profile`.`userid` 
                     WHERE `{tbl_prefix}user_profile`.`user_profile_id` IS NULL AND `{tbl_prefix}users`.`userid` != 0';
