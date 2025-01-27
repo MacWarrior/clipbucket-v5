481a8069ec448b9c22358ecac43d654d02e94f89
clement-sanz <clement@oxygenz.fr>
2025-01-27 16:54:40 +0100

    402 fixes

---
+++ upload/cb_install/sql/5.5.1/MWIP.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace V5_5_1;
+require_once \DirPath::get('classes') . DIRECTORY_SEPARATOR . 'migration' . DIRECTORY_SEPARATOR . 'migration.class.php';
+
+class MWIP extends \Migration
+{
+    /**
+     * @throws \Exception
+     */
+    public function start()
+    {
+        self::generateTranslation('share', [
+            'fr' => 'Partager',
+            'en' => 'Share'
+        ]);
+        self::generateTranslation('enable_link_sharing', [
+            'fr'=>'Activer le partage par lien',
+            'en'=>'Enable link sharing'
+        ]);
+        self::generateTranslation('enable_internal_sharing', [
+            'fr'=>'Activer le partage en interne',
+            'en'=>'Enable internal sharing'
+        ]);
+
+        self::generateConfig('enable_collection_link_sharing', 'yes');
+        self::generateConfig('enable_collection_internal_sharing', 'yes');
+    }
+}
