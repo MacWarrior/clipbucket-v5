98404e2cccf077dd418a51dda150981bbf0686bd
Clement Sanz <clement.oxygenz.fr>
2025-02-27 16:47:19 +0100

    432 fix playlist

---
+++ upload/cb_install/sql/5.5.1/MWIP.php
@@ -0,0 +1,17 @@
+<?php
+namespace V5_5_1;
+require_once \DirPath::get('classes') . 'migration' . DIRECTORY_SEPARATOR . 'migration.class.php';
+
+class MWIP extends \Migration
+{
+    /**
+     * @throws \Exception
+     */
+    public function start()
+    {
+        self::alterTable('ALTER TABLE ' . tbl('playlists') . ' DROP COLUMN tags', [
+            'table'=>'playlists',
+            'column'=>'tags',
+        ] ,[]);
+    }
+}
