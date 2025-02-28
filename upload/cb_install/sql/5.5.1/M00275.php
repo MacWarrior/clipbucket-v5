545ff0c2d0d42205fa7c01bb1ded5143697b149f
MacWarrior <macwarrior94@gmail.com>
2025-02-28 14:39:39 +0100

    Update revision & changelog

--- upload/cb_install/sql/5.5.1/M00275.php
+++ upload/cb_install/sql/5.5.1/M00275.php
@@ -3,7 +3,7 @@
 namespace V5_5_1;
 require_once \DirPath::get('classes') . DIRECTORY_SEPARATOR . 'migration' . DIRECTORY_SEPARATOR . 'migration.class.php';
 
-class MWIP extends \Migration
+class M00275 extends \Migration
 {
     /**
      * @throws \Exception
