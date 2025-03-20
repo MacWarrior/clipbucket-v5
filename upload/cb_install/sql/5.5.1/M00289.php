6fde78149f9101faa0d344b0a3a599a028db2bde
MacWarrior <macwarrior94@gmail.com>
2025-03-20 10:55:27 +0100

    Update revision & changelog

--- upload/cb_install/sql/5.5.1/M00289.php
+++ upload/cb_install/sql/5.5.1/M00289.php
@@ -3,14 +3,13 @@
 namespace V5_5_1;
 require_once \DirPath::get('classes') . DIRECTORY_SEPARATOR . 'migration' . DIRECTORY_SEPARATOR . 'migration.class.php';
 
-class MWIP extends \Migration
+class M00289 extends \Migration
 {
     /**
      * @throws \Exception
      */
     public function start()
     {
-
         self::generateTranslation('cannot_delete_not_empty_category', [
             'fr' => 'Vous ne pouvez pas supprimer une catégorie associée à des éléments',
             'en' => 'You cannot delete a category which is linked to an item'
@@ -21,7 +20,7 @@ class MWIP extends \Migration
             'en' => 'Collapsible categories'
         ]);
 
-        $thumbs = self::req('select category_id, category_thumb FROM ' . tbl('categories') . ' WHERE TRIM(category_thumb) != \'\'');
+        $thumbs = self::req('SELECT category_id, category_thumb FROM ' . tbl('categories') . ' WHERE TRIM(category_thumb) != \'\'');
         foreach ($thumbs as $thumb) {
             $ext = pathinfo($thumb['category_thumb'], PATHINFO_EXTENSION);
             self::query('UPDATE ' . tbl('categories') . ' SET category_thumb = \'' . $thumb['category_id'] . '.' . $ext . '\' 
