ea23304bb165a61d1e015574697f59e7d1576ee3
clement-sanz <clement@oxygenz.fr>
2025-03-31 10:00:18 +0200

    466 add mwip

---
+++ upload/cb_install/sql/5.5.1/MWIP.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace V5_5_1;
+require_once \DirPath::get('classes') . DIRECTORY_SEPARATOR . 'migration' . DIRECTORY_SEPARATOR . 'migration.class.php';
+
+class MWIP extends \Migration
+{
+    /**
+     * @throws \Exception
+     */
+    public function start()
+    {
+
+        self::generateConfig('enable_video_categories', 'no');
+    }
+}
