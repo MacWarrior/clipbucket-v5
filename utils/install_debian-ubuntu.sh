287b341d33f7b7125f0243b121c31a8a0b77f795
MacWarrior <macwarrior94@gmail.com>
2025-04-22 18:25:04 +0200

    Fix subdir vhost & htaccess

--- utils/install_debian-ubuntu.sh
+++ utils/install_debian-ubuntu.sh
@@ -340,19 +340,13 @@ server {
         fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
     }
 
-    # set expiration of assets to MAX for caching
-    location ~* \.(ico|css|js)(\?[0-9]+)?$ {
-        expires max;
-        log_not_found off;
-    }
-
     error_page 404 /404;
     error_page 403 /403;
     location ~* ^(.*/)?403$ {
-        try_files $uri $1/403.php;
+        try_files $uri $1403.php;
     }
     location ~* ^(.*/)?404$ {
-        try_files $uri $1/404.php;
+        try_files $uri $1404.php;
     }
 
     # Forbidden access
@@ -367,104 +361,93 @@ server {
     location ~* ^(.*/)?files/ {
         try_files $uri $uri/ =404;
     }
+    location ~* \.(css|js|jpe?g|png|gif|webp|svg|ico|woff2?|ttf|eot|otf|mp4|m3u8|ts|srt|webm|ogg|mp3)(\?[0-9]+)?$ {
+        access_log off;
+        log_not_found off;
+        expires max;
+        try_files $uri =404;
+    }
 
     # External use
     location ~* ^(.*/)?sitemap.xml$ {
         rewrite ^(.*/)?sitemap.xml$ $1sitemap.php last;
-        break;
     }
     location ~* ^(.*/)?rss(/([a-zA-Z0-9][^/]*))?$ {
         rewrite ^(.*/)?rss/?$ $1rss.php?$query_string last;
         rewrite ^(.*/)?rss/([a-zA-Z0-9][^/]*)$ $1rss.php?mode=$2&$query_string last;
-        break;
     }
 
     # Collections
     location ~* ^(.*/)?collections(/(.*))?$ {
-        rewrite ^(.*/)?collections/(.*)/(.*)/(.*)/(.*) $1collections.php?cat=$1&sort=$2&time=$3&page=$4&$query_string last;
-        rewrite ^(.*/)?collections/(.*)/(.*)/(.*) $1collections.php?sort=$1&time=$2&page=$3&$query_string last;
+        rewrite ^(.*/)?collections/(.*)/(.*)/(.*)/(.*) $1collections.php?cat=$2&sort=$3&time=$4&page=$5&$query_string last;
+        rewrite ^(.*/)?collections/(.*)/(.*)/(.*) $1collections.php?sort=$2&time=$3&page=$4&$query_string last;
         rewrite ^(.*/)?collections/([0-9]+) $1collections.php?page=$2&$query_string last;
-        rewrite ^(.*/)?collections/?$ $1collections.php last;
-        break;
+        rewrite ^(.*/)?collections/?$ $1collections.php?$query_string last;
     }
     location ~* ^(.*/)?collection(/(.*))?$ {
         rewrite ^(.*/)?collection/(.*)/(.*)/(.*) $1view_collection.php?cid=$2&type=$3&page=$4&$query_string last;
-        break;
     }
 
     # Videos
     location ~* ^(.*/)?videos(/(.*))?$ {
-        rewrite ^(.*/)?videos/(.*)/(.*)/(.*)/(.*) $1videos.php?cat=$1&sort=$2&time=$3&page=$4&$query_string last;
-        rewrite ^(.*/)?videos/(.*)/(.*)/(.*) $1videos.php?sort=$1&time=$2&page=$3&$query_string last;
+        rewrite ^(.*/)?videos/(.*)/(.*)/(.*)/(.*) $1videos.php?cat=$2&sort=$3&time=$4&page=$5&$query_string last;
+        rewrite ^(.*/)?videos/(.*)/(.*)/(.*) $1videos.php?sort=$2&time=$3&page=$4&$query_string last;
         rewrite ^(.*/)?videos/([0-9]+) $1videos.php?page=$2&$query_string last;
         rewrite ^(.*/)?videos/?$ $1videos.php?$query_string last;
-        break;
     }
     location ~* ^(.*/)?video(/(.*))?$ {
         rewrite ^(.*/)?video/(.*)/(.*) $1watch_video.php?v=$2&$query_string last;
         rewrite ^(.*/)?video/([0-9]+)_(.*) $1watch_video.php?v=$2&$query_string last;
-        break;
     }
     location ~* ^(/.*/)?(.+)_v([0-9]+)$ {
         rewrite ^(.*/)?(.*)_v([0-9]+) $1watch_video.php?v=$3&$query_string last;
-        break;
     }
 
     # Photos
     location ~* ^(.*/)?item(/(.*))?$ {
         rewrite ^(.*/)?item/(.*)/(.*)/(.*)/(.*) $1view_item.php?item=$4&type=$2&collection=$3&$query_string last;
-        break;
     }
     location ~* ^(.*/)?photos(/(.*))?$ {
-        rewrite ^(.*/)?photos/(.*)/(.*)/(.*)/(.*) $1photos.php?cat=$1&sort=$2&time=$3&page=$4&$query_string last;
-        rewrite ^(.*/)?photos/(.*)/(.*)/(.*) $1photos.php?sort=$1&time=$2&page=$3&$query_string last;
+        rewrite ^(.*/)?photos/(.*)/(.*)/(.*)/(.*) $1photos.php?cat=$2&sort=$3&time=$4&page=$5&$query_string last;
+        rewrite ^(.*/)?photos/(.*)/(.*)/(.*) $1photos.php?sort=$2&time=$3&page=$4&$query_string last;
         rewrite ^(.*/)?photos/([0-9]+) $1photos.php?page=$2&$query_string last;
         rewrite ^(.*/)?photos/?$ $1photos.php?$query_string last;
-        break;
     }
 
     # Channels
     location ~* ^(.*/)?channels(/(.*))?$ {
-        rewrite ^(.*/)?channels/(.*)/(.*)/(.*)/(.*) $1channels.php?cat=$1&sort=$2&time=$3&page=$4&$query_string last;
-        rewrite ^(.*/)?channels/(.*)/(.*)/(.*) $1channels.php?sort=$1&time=$2&page=$3&$query_string last;
+        rewrite ^(.*/)?channels/(.*)/(.*)/(.*)/(.*) $1channels.php?cat=$2&sort=$3&time=$4&page=$5&$query_string last;
+        rewrite ^(.*/)?channels/(.*)/(.*)/(.*) $1channels.php?sort=$2&time=$3&page=$4&$query_string last;
         rewrite ^(.*/)?channels/([0-9]+) $1channels.php?page=$2&$query_string last;
         rewrite ^(.*/)?channels/?$ $1channels.php?$query_string last;
-        break;
     }
     location ~* ^(.*/)?user/(.*)$ {
         rewrite ^(.*/)?user/(.*) $1view_channel.php?user=$2&$query_string last;
-        break;
     }
 
     # Uploads
     location ~* ^(.*/)?photo_upload(/(.*))?$ {
         rewrite ^(.*/)?photo_upload/(.*)$ $1photo_upload.php?collection=$2&$query_string last;
         rewrite ^(.*/)?photo_upload/?$ $1photo_upload.php?$query_string last;
-        break;
     }
     location ~* ^(.*/)?upload/?$ {
         rewrite ^(.*/)?upload/?$ $1upload.php?$query_string last;
-        break;
     }
 
     # Account
     location ~* ^(.*/)?my_account$ {
         rewrite ^(.*/)?my_account$ $1myaccount.php?$query_string last;
-        break;
     }
     location ~* ^(.*/)?signup/?$ {
         rewrite ^(.*/)?signup/?$ $1signup.php?$query_string last;
-        break;
     }
     location ~* ^(.*/)?signin/?$ {
         rewrite ^(.*/)?signin/?$ $1signup.php?mode=login&$query_string last;
-        break;
     }
 
     # Custom pages
     location ~* ^(.*/)?page/([0-9]+)/(.*)$ {
         rewrite ^(.*/)?page/([0-9]+)/(.*) $1view_page.php?pid=$2&$query_string last;
-        break;
     }
 }
 EOF
