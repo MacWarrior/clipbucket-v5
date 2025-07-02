59f21ae7db708f6078f4fe01e9678608a06666b0
clement-sanz <clement@oxygenz.fr>
2025-07-02 09:18:04 +0200

    500 move old folders

--- upload/js/functions.js
+++ upload/js/functions.js
@@ -1,4 +1,4 @@
-var page = baseurl+'ajax.php';
+var page = baseurl+'actions/ajax.php';
 var loading_img = "<img style='vertical-align:middle' src='"+imageurl+"/ajax-loader.gif'>";
 var loading = loading_img+" Loading...";
 
@@ -32,7 +32,7 @@ function load_more(limit,mode,inner_mode,append_id,attrb,cat_id,total)
             $('#'+inner_mode).html('loading');
         },
         type: 'POST',
-        url: baseurl+'ajax.php',
+        url: baseurl+'actions/ajax.php',
         data: {
             limit : limit,
             mode : mode,
@@ -923,7 +923,7 @@ function checkUncheckAll(theElement) {
  */
 function rate(id,rating,type)
 {
-    var page = baseurl+'ajax.php';
+    var page = baseurl+'actions/ajax.php';
     $.post(page, {
         mode : 'rating',
         id:id,
