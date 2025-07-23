2cd276eb99934c03427ebcfaf889df7ab8d217d4
clement-sanz <clement@oxygenz.fr>
2025-07-23 13:54:06 +0200

    457 update curl to send file

--- upload/includes/classes/curl.class.php
+++ upload/includes/classes/curl.class.php
@@ -6,6 +6,7 @@ class Curl
     private $base_url;
 
     private $curl;
+    private $headers;
 
     /**
      * @param $base_url
@@ -15,6 +16,10 @@ class Curl
     {
         $this->base_url = $base_url . (substr($base_url, -1) != '/' ? '/' : '');
         $this->curl = curl_init();
+        $this->headers = [
+            'Authorization: Bearer ' . $beared_token,
+            'accept: application/json'
+        ];
         curl_setopt_array($this->curl, [
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING       => '',
@@ -22,10 +27,7 @@ class Curl
             CURLOPT_TIMEOUT        => 30,
             CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST  => 'GET',
-            CURLOPT_HTTPHEADER     => [
-                'Authorization: Bearer ' . $beared_token,
-                'accept: application/json'
-            ]
+            CURLOPT_HTTPHEADER     => $this->headers
         ]);
     }
 
@@ -38,6 +40,29 @@ class Curl
     {
         curl_setopt_array($this->curl, [
             CURLOPT_URL => $this->base_url . $action . (!empty($param) ? '?' : '') . http_build_query($param)
+        ]);
+        $response = curl_exec($this->curl);
+        $err = curl_error($this->curl);
+        return ['error'=>$err, 'response'=> json_decode($response,true)];
+    }
+
+    /**
+     * Upload a file using POST request
+     * @param string $action Endpoint to call
+     * @param string $filePath Path to the file to send
+     * @param array $params Additional POST parameters
+     * @param string $fieldName Name of the file field
+     * @return array
+     */
+    public function sendFile(string $action, string $filePath, array $params = [], string $fieldName = 'file'): array
+    {
+        $params[$fieldName] = new \CURLFile($filePath);
+        curl_setopt_array($this->curl, [
+            CURLOPT_URL => $this->base_url . $action,
+            CURLOPT_POST => true,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $params,
+            CURLOPT_HTTPHEADER => array_merge($this->headers, ['Content-Type: multipart/form-data'])
         ]);
         $response = curl_exec($this->curl);
         $err = curl_error($this->curl);
