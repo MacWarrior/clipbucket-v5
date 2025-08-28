2f76a148d0614da9ecc4482c2cb1546910f34698
MacWarrior <macwarrior94@gmail.com>
2025-08-28 17:24:20 +0200

    Front office : Implement fullwidth videos page

---
+++ upload/cb_install/sql/5.5.2/M00133.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace V5_5_2;
+require_once \DirPath::get('classes') . DIRECTORY_SEPARATOR . 'migration' . DIRECTORY_SEPARATOR . 'migration.class.php';
+
+class M00133 extends \Migration
+{
+    /**
+     * @throws \Exception
+     */
+    public function start()
+    {
+        self::generateConfig('videos_enable_fullwidth', 'no');
+    }
+
+}
