87e02126a400db699fa5dc4edaa9624432cb497c
MacWarrior <macwarrior94@gmail.com>
2025-10-20 15:31:13 +0200

    Update revision & changelog, update translation

--- upload/cb_install/sql/5.5.2/M00150.php
+++ upload/cb_install/sql/5.5.2/M00150.php
@@ -3,7 +3,7 @@
 namespace V5_5_2;
 require_once \DirPath::get('classes') . DIRECTORY_SEPARATOR . 'migration' . DIRECTORY_SEPARATOR . 'migration.class.php';
 
-class MWIP extends \Migration
+class M00150 extends \Migration
 {
     /**
      * @throws \Exception
@@ -11,8 +11,8 @@ class MWIP extends \Migration
     public function start()
     {
         self::generateTranslation('video_title_exceed', [
-            'fr'=>'Le titre de la vidéo, %s, dépasse le nombre de caractères autorisés. (%s)',
-            'en'=>'Video title, %s, exceeds the maximum length. (%s)'
+            'fr'=>'Le titre de la vidéo, "%s", dépasse le nombre de caractères autorisés. (%s)',
+            'en'=>'Video title, "%s", exceeds the maximum length. (%s)'
         ]);
     }
 
--- upload/cb_install/sql/language_FRA.sql
+++ upload/cb_install/sql/language_FRA.sql
@@ -2710,4 +2710,4 @@ VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `languag
 INSERT INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
 VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'no_lock_to_delete'), 'Aucun verrou de conversion à supprimer', @language_id);
 INSERT INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
-VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'video_title_exceed'), 'Le titre de la vidéo, %s, dépasse le nombre de caractères autorisés. (%s)', @language_id);
+VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'video_title_exceed'), 'Le titre de la vidéo, "%s", dépasse le nombre de caractères autorisés. (%s)', @language_id);
