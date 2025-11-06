1c9ced50792ba6c95fd0fa9ea996cb92fa544b4e
MacWarrior <macwarrior94@gmail.com>
2025-11-06 13:27:11 +0100

    Update revision & changelog

--- upload/cb_install/sql/5.5.2/M00161.php
+++ upload/cb_install/sql/5.5.2/M00161.php
@@ -3,14 +3,13 @@
 namespace V5_5_2;
 require_once \DirPath::get('classes') . DIRECTORY_SEPARATOR . 'migration' . DIRECTORY_SEPARATOR . 'migration.class.php';
 
-class MWIP extends \Migration
+class M00161 extends \Migration
 {
     /**
      * @throws \Exception
      */
     public function start()
     {
-
         if (config('photo_main_list') == 10) {
             self::updateConfig('photo_main_list', 9);
         }
