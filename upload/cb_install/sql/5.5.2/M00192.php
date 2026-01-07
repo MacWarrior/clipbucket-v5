3ca3ddffc89c8b69f9803d01d1ea579c70e732b3
MacWarrior <macwarrior94@gmail.com>
2026-01-07 11:48:38 +0100

    Update revision & changelog

--- upload/cb_install/sql/5.5.2/M00192.php
+++ upload/cb_install/sql/5.5.2/M00192.php
@@ -3,7 +3,7 @@
 namespace V5_5_2;
 require_once \DirPath::get('classes') . DIRECTORY_SEPARATOR . 'migration' . DIRECTORY_SEPARATOR . 'migration.class.php';
 
-class MWIP extends \Migration
+class M00192 extends \Migration
 {
     /**
      * @throws \Exception
@@ -14,7 +14,6 @@ class MWIP extends \Migration
             'fr' => 'Votre version PHP actuelle (%s) n’est pas supportée par la mise à jour ciblée (%s). La mise à jour vers cette version pourrait endommager votre site web ; êtes-vous sûr de vouloir effectuer la mise à jour ?',
             'en' => 'Your current PHP version (%s) isn’t supported with targeted update (%s). Updating to this version may break your website ; are you sure you want to update ?'
         ]);
-
     }
 
 }
