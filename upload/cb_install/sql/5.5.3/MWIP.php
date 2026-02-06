84fae54093f5a5a74703e968f69a0018212f886f
clement-sanz <clement@oxygenz.fr>
2026-02-06 15:15:44 +0100

    756 fix quotes

--- upload/cb_install/sql/5.5.3/MWIP.php
+++ upload/cb_install/sql/5.5.3/MWIP.php
@@ -10,13 +10,13 @@ class MWIP extends \Migration
      */
     public function start()
     {
-        $sql = 'SELECT MIN(configid) as config_id, name FROM {tbl_prefix}config WHERE 1 GROUP BY name HAVING COUNT(name) > 1';
+        $sql = 'SELECT MIN(configid) as config_id, name FROM {tbl_prefix}config WHERE 1 GROUP BY `name` HAVING COUNT(`name`) > 1';
         $configs = self::req($sql);
         foreach ($configs as $config) {
-            self::query('DELETE FROM {tbl_prefix}config WHERE configid != ' . $config['config_id'] . ' AND name = \'' . $config['name'] . '\'');
+            self::query('DELETE FROM {tbl_prefix}config WHERE configid != ' . $config['config_id'] . ' AND `name` = \'' . $config['name'] . '\'');
         }
 
-        self::alterTable('ALTER TABLE {tbl_prefix}config ADD  CONSTRAINT `name` UNIQUE(`name`)', [
+        self::alterTable('ALTER TABLE {tbl_prefix}config ADD CONSTRAINT `name` UNIQUE(`name`)', [
             'table' => 'config',
             'column' => 'name'
         ], [
