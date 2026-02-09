b89a346db8d2a865ea14474241471487ff12a4cf
MacWarrior <macwarrior94@gmail.com>
2026-02-09 10:19:31 +0100

    Update revision & changelog

--- upload/cb_install/sql/5.5.3/M00038.php
+++ upload/cb_install/sql/5.5.3/M00038.php
@@ -3,20 +3,20 @@
 namespace V5_5_3;
 require_once \DirPath::get('classes') . DIRECTORY_SEPARATOR . 'migration' . DIRECTORY_SEPARATOR . 'migration.class.php';
 
-class MWIP extends \Migration
+class M00038 extends \Migration
 {
     /**
      * @throws \Exception
      */
     public function start()
     {
-        $sql = 'SELECT MIN(configid) as config_id, name FROM {tbl_prefix}config WHERE 1 GROUP BY `name` HAVING COUNT(`name`) > 1';
+        $sql = 'SELECT MIN(`configid`) as `config_id`, `name` FROM `{tbl_prefix}config` WHERE 1 GROUP BY `name` HAVING COUNT(`name`) > 1';
         $configs = self::req($sql);
         foreach ($configs as $config) {
-            self::query('DELETE FROM {tbl_prefix}config WHERE configid != ' . $config['config_id'] . ' AND `name` = \'' . $config['name'] . '\'');
+            self::query('DELETE FROM `{tbl_prefix}config` WHERE `configid` != ' . $config['config_id'] . ' AND `name` = \'' . $config['name'] . '\'');
         }
 
-        self::alterTable('ALTER TABLE {tbl_prefix}config ADD CONSTRAINT `name` UNIQUE(`name`)', [
+        self::alterTable('ALTER TABLE `{tbl_prefix}config` ADD CONSTRAINT `name` UNIQUE(`name`)', [
             'table' => 'config',
             'column' => 'name'
         ], [
