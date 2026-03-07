f3580b9d657765417558341efab9cff4b4477d95
nvanhaezebrouck <113363985+nvanhaezebrouck@users.noreply.github.com>
2026-03-07 17:14:59 +0100

    correction config xhgui

--- docker/xhgui-config.php
+++ docker/xhgui-config.php
@@ -1,38 +1,21 @@
 <?php
 
 return [
-    'profiler' => [
-        'enable' => true,
-        'generator' => 'uuidV4',
-    ],
-    'profiler.exclude' => [],
+
+    // On force PDO (MySQL)
     'save.handler' => 'pdo',
-    'save.handler.file' => [
-        'filename' => '/tmp/xhgui/profiles/profiles.dat',
-    ],
-    'save.handler.pdo' => [
-        'instance' => null,
+
+    'pdo' => [
         'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=xhgui;charset=utf8mb4',
         'user' => 'xhgui',
         'pass' => 'xhgui',
+
+        // noms des tables attendus
         'table' => 'results',
         'tableWatch' => 'watches',
-        'initSchema' => true,
+        'initSchema' => true
     ],
-    'save.handler.mongodb' => [
-        'hostname' => '127.0.0.1',
-        'port' => '27017',
-        'username' => null,
-        'password' => null,
-        'database' => 'xhprof',
-        'options' => [
-            'connector' => 'mongodb',
-        ],
-    ],
-    'page.limit' => 25,
-    'detail.count' => 6,
-    'date.format' => 'Y-m-d H:i:s',
-    'templates' => __DIR__ . '/../templates',
-    'cache' => __DIR__ . '/../cache',
+
     'timezone' => 'Europe/Paris',
+    'date.format' => 'Y-m-d H:i:s',
 ];
