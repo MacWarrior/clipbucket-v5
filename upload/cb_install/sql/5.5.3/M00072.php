ab1ada44c5ae08fbb16cd30be4a0685c6911afee
clement-sanz <clement@oxygenz.fr>
2026-03-12 16:32:52 +0100

    789 manage recent format thumb ; update only detected files

--- upload/cb_install/sql/5.5.3/M00072.php
+++ upload/cb_install/sql/5.5.3/M00072.php
@@ -25,7 +25,7 @@ class M00072 extends \Migration
             foreach ($vid_thumbs as $thumb) {
                 $files_info = [];
                 //pattern must match :  /`file_name`-`size`-`num`.`extension`
-                preg_match('/\/\w*-(\w{1,16})-(\d{1,3})\.(\w{2,4})$/', $thumb, $files_info);
+                preg_match('/\/\w*-(\w{1,16})-(\d{1,4})\.(\w{2,4})$/', $thumb, $files_info);
                 if (!empty($files_info)) {
                     if (\Update::IsCurrentDBVersionIsHigherOrEqualTo('5.5.3', '14')) {
                         $sql = 'SELECT id_video_image FROM ' . tbl('video_image') . ' WHERE videoid = ' . $video['videoid'] . ' AND type = \'thumbnail\' AND num = ' . (int)$files_info[2];
@@ -49,41 +49,27 @@ class M00072 extends \Migration
                         } else {
                             $sizes = \VideoThumbs::getWidthHeightFromSize($files_info[1]);
                         }
-                        \Clipbucket_db::getInstance()->insert(tbl('video_thumb'), [
-                            'id_video_image',
-                            'width',
-                            'height',
-                            'extension',
-                            'version',
-                            'is_original_size'
-                        ], [
-                            $id_video_image,
-                            $sizes['width'] ?? null,
-                            $sizes['height'] ?? null,
-                            $files_info[3],
-                            $video['video_version'],
-                            (int)($files_info[1] == 'original')
-                        ], ignore: true);
 
-                    } else {
-                        \Clipbucket_db::getInstance()->insert(tbl('video_thumbs'), [
-                            'videoid',
-                            'resolution',
-                            'num',
-                            'extension',
-                            'version'
-                        ], [
-                            $video['videoid'],
-                            $files_info[1],
-                            $files_info[2],
-                            $files_info[3],
-                            $video['video_version']
-                        ], ignore: true);
+                        $sql = ' REPLACE INTO ' . tbl('video_thumb') . ' (`id_video_image`,
+                            `width`,
+                            `height`,
+                            `extension`,
+                            `version`,
+                            `is_original_size`) VALUES (
+                            ' . $id_video_image . ',
+                            ' . ($sizes['width'] ?? 0) . ',
+                            ' . ($sizes['height'] ?? 0) . ',
+                            \'' . $files_info[3] . '\',
+                            \'' . ($video['video_version'] <= '5.5.2' ? $video['video_version'] : '5.5.2' ). '\',
+                            ' . (int)($files_info[1] == 'original') . ' )';
+                        error_log($sql);
+                        self::query($sql);
                     }
                 }
             }
         }
     }
+
     /**
      * @throws \Exception
      */
@@ -92,7 +78,7 @@ class M00072 extends \Migration
         $limit = 100;
         $offset = 0;
         $video_done = [];
-        $thumbs = new \GlobIterator(\DirPath::get('thumbs') . '[0-9]*/[0-9]*/[0-9]*/*.*' );
+        $thumbs = new \GlobIterator(\DirPath::get('thumbs') . '[0-9]*/[0-9]*/[0-9]*/*.*');
         foreach ($thumbs as $thumb) {
             if (!in_array(pathinfo($thumb)['extension'], ['jpg', 'png', 'jpeg', 'gif'])) {
                 continue;
@@ -107,19 +93,7 @@ class M00072 extends \Migration
             }
             $video = \Video::getInstance()->getOne(['file_name' => $video_file_name]);
             if (!empty($video)) {
-                //count
-                $nb_video_image = \VideoThumbs::getAll(['videoid' => $video['videoid'], 'count'=>true]);
                 self::importOldThumbFromDisk($video);
-                $nb_video_image_after_import = \VideoThumbs::getAll(['videoid' => $video['videoid'], 'count'=>true]);
-
-                if ($nb_video_image_after_import != $nb_video_image) {
-                    continue;
-                }
-                $sql = 'UPDATE ' . tbl('video_image') . ' VI
-                INNER JOIN ' . tbl('video_thumb') . ' VT ON VT.id_video_image = VI.id_video_image
-                SET VT.version = \'' . $video['video_version'] . '\'
-                WHERE VI.videoid = ' . $video['videoid'] . ' AND VI.type = \'thumbnail\'';
-                self::query($sql);
 
                 //delete duplicate
                 $sql_delete = 'DELETE vt_old
