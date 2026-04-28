3548ff7d26faecc43f28fd979e767b82f9a9a438
MacWarrior <macwarrior94@gmail.com>
2026-04-28 11:26:04 +0200

    Update revision & migration

--- upload/cb_install/sql/5.5.3/M00117.php
+++ upload/cb_install/sql/5.5.3/M00117.php
@@ -134,63 +134,6 @@ class M00117 extends \Migration
             'en' => 'Dislikes',
         ]);
 
-        self::alterTable('ALTER TABLE `{tbl_prefix}video` DROP COLUMN `rated_by`;', [
-            'table'  => 'video',
-            'column' => 'rated_by'
-        ]);
-        self::alterTable('ALTER TABLE `{tbl_prefix}video` DROP COLUMN `voter_ids`;', [
-            'table'  => 'video',
-            'column' => 'voter_ids'
-        ]);
-        self::alterTable('ALTER TABLE `{tbl_prefix}video` DROP COLUMN `rating`;', [
-            'table'  => 'video',
-            'column' => 'rating'
-        ]);
-
-        self::alterTable('ALTER TABLE `{tbl_prefix}user_profile` DROP COLUMN `rated_by`;', [
-            'table'  => 'user_profile',
-            'column' => 'rated_by'
-        ]);
-        self::alterTable('ALTER TABLE `{tbl_prefix}user_profile` DROP COLUMN `voters`;', [
-            'table'  => 'user_profile',
-            'column' => 'voters'
-        ]);
-        self::alterTable('ALTER TABLE `{tbl_prefix}user_profile` DROP COLUMN `rating`;', [
-            'table'  => 'user_profile',
-            'column' => 'rating'
-        ]);
-
-        self::alterTable('ALTER TABLE `{tbl_prefix}collections` DROP COLUMN `rated_by`;', [
-            'table'  => 'collections',
-            'column' => 'rated_by'
-        ]);
-        self::alterTable('ALTER TABLE `{tbl_prefix}collections` DROP COLUMN `voters`;', [
-            'table'  => 'collections',
-            'column' => 'voter_ids'
-        ]);
-        self::alterTable('ALTER TABLE `{tbl_prefix}collections` DROP COLUMN `rating`;', [
-            'table'  => 'collections',
-            'column' => 'rating'
-        ]);
-
-        self::alterTable('ALTER TABLE `{tbl_prefix}photos` DROP COLUMN `rated_by`;', [
-            'table'  => 'photos',
-            'column' => 'rated_by'
-        ]);
-        self::alterTable('ALTER TABLE `{tbl_prefix}photos` DROP COLUMN `voters`;', [
-            'table'  => 'photos',
-            'column' => 'voter_ids'
-        ]);
-        self::alterTable('ALTER TABLE `{tbl_prefix}photos` DROP COLUMN `rating`;', [
-            'table'  => 'photos',
-            'column' => 'rating'
-        ]);
-
-        self::alterTable('ALTER TABLE `{tbl_prefix}users` DROP COLUMN `voted`;', [
-            'table'  => 'users',
-            'column' => 'voted'
-        ]);
-
         self::insertTool('recalcul_rates', 'AdminTool::recalculRates', '0 0 1 * *', true);
 
         self::generateTranslation('recalcul_rates_label', [
