4966d3a2113735647ce7bef872fa0cb6c4278f53
clement-sanz <clement@oxygenz.fr>
2026-04-28 15:26:33 +0200

    728 fix tool install

--- upload/cb_install/sql/configs.sql
+++ upload/cb_install/sql/configs.sql
@@ -385,7 +385,7 @@ INSERT INTO `{tbl_prefix}tools` (`language_key_label`, `language_key_description
     ('regenerate_all_video_thumbs_label', 'regenerate_all_video_thumbs_description', 'AdminTool::regenerateAllVideoThumbs', 'regenerate_all_video_thumbs', NULL, NULL, '1', '0'),
     ('update_tmdb_info_label', 'update_tmdb_info_description', 'AdminTool::updateTmdbInfo', 'update_tmdb_info', NULL, NULL, '1', '0'),
     ('task_queue_cleaner_label', 'task_queue_cleaner_description', 'AdminTool::cleanUpTask', 'task_queue_cleaner', '0 0 1 * *', CURRENT_TIMESTAMP, '1', '0'),
-    ('recalcul_rates_label_label', 'recalcul_rates_label_description', 'AdminTool::recalculRates', 'recalcul_rates_label', '0 0 1 * *', CURRENT_TIMESTAMP, '1', '0');
+    ('recalcul_rates_label', 'recalcul_rates_description', 'AdminTool::recalculRates', 'recalcul_rates_label', '0 0 1 * *', CURRENT_TIMESTAMP, '1', '0');
 
 INSERT INTO `{tbl_prefix}tags_type` (`name`) VALUES ('video'), ('photo'), ('collection'), ('profile'), ('playlist'), ('actors'), ('producer'), ('executive_producer'), ('director'), ('crew'), ('genre');
 
