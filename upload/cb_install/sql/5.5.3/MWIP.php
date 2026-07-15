cd40891b713c5e640ca01ee65e403e6108708332
clement-sanz <clement@oxygenz.fr>
2026-07-15 09:39:52 +0200

    932 add missing translations; fix subtitle deletion

---
+++ upload/cb_install/sql/5.5.3/MWIP.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace V5_5_3;
+
+require_once \DirPath::get('classes') . DIRECTORY_SEPARATOR . 'migration' . DIRECTORY_SEPARATOR . 'migration.class.php';
+
+class MWIP extends \Migration
+{
+    /**
+     * @throws \Exception
+     */
+    public function start()
+    {
+       self::generateTranslation('invalid_params', [
+           'fr'=>'Certains paramètres ne sont pas valides',
+           'en'=>'Some parameters are invalid'
+       ]);
+
+    }
+}
