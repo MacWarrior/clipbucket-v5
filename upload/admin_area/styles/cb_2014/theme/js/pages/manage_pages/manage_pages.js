d6e6360ef6156608c43d476f70ad3380e2656c29
csanz <clement@oxygenz.fr>
2026-08-31 12:44:06 +0200

    Back office : Implement pages translation, Implement languages flags selection
    ---------
    Co-authored-by: MacWarrior <macwarrior94@gmail.com>

--- upload/admin_area/styles/cb_2014/theme/js/pages/manage_pages/manage_pages.js
+++ upload/admin_area/styles/cb_2014/theme/js/pages/manage_pages/manage_pages.js
@@ -8,13 +8,13 @@ $(document).ready(function () {
         $(obj).next('span').remove();
     }
 
-    $('input#page_name,input#page_title').on('keyup', function () {
+    $('input#page_name').on('keyup', function () {
         let input = $(this);
         let input_val = input.val();
         if (input_val === '') {
             addErrClass(input, errors["empty_"+input.attr('id')], true, false);
             has_error = true;
-        } else if (input_val.indexOf(' ') >= 0 && input.attr('id') === 'page_name') {
+        } else if (input_val.indexOf(' ') >= 0) {
             addErrClass(input, errors["page_name_cant_have_space"], true, false);
             has_error = true;
         } else {
@@ -31,5 +31,18 @@ $(document).ready(function () {
             return false;
         }
         $('#page_content').summernote('code', $('#page_content').summernote('code'));
+    });
+
+    $('.flag_language').on('click', function () {
+        const id = $(this).data('id');
+        $('#selected_lang').val(parseInt(id));
+        $('.flag_language img').removeClass('selected');
+        $('.flag_language[data-id="' + id + '"] img').addClass('selected');
+        const form = $(this).parents('form');
+        const textareas = form.find('textarea');
+        textareas.hide();
+        form.find('input[name^="page_title"]').hide();
+        $('#page_content' + id).show();
+        $('#page_title' + id).show();
     });
 });
--- upload/cb_install/sql/language_ENG.sql
+++ upload/cb_install/sql/language_ENG.sql
@@ -1,7 +1,7 @@
 SET @language_id = 1;
 
-INSERT INTO `{tbl_prefix}languages` (`language_id`, `language_name`, `language_active`, `language_default`, `language_code`)
-VALUES (@language_id, 'English', 'yes', 'yes','en');
+INSERT INTO `{tbl_prefix}languages` (`language_id`, `language_name`, `language_active`, `language_default`, `language_code`, `language_flag`)
+VALUES (@language_id, 'English', 'yes', 'yes','en', 'us');
 
 INSERT INTO `{tbl_prefix}languages_translations` (`language_id`, `id_language_key`, `translation`)
 VALUES (@language_id, (SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE language_key = 'ad_name_error'), 'Please enter a name for the Advertisment');
@@ -3881,3 +3881,23 @@ INSERT INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translatio
 VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'original_video_files'), 'Original video files', @language_id);
 INSERT INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
 VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'invalid_params'), 'Some parameters are invalid', @language_id);
+INSERT INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
+VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'lang_flag_empty'), 'Please fill the language flag field', @language_id);
+INSERT INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
+VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'option_display_language_flag'), 'Display language flag', @language_id);
+INSERT INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
+VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'edit_page_translation'), 'Edit translation : %s', @language_id);
+INSERT INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
+VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'no_specified'), 'Not specified', @language_id);
+INSERT INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
+VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'specified'), 'Specified', @language_id);
+INSERT INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
+VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'new_page'), 'Create a new page', @language_id);
+INSERT INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
+VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'update_page'), 'Update page', @language_id);
+INSERT INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
+VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'edit_page'), 'Edit page', @language_id);
+INSERT INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
+VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'translations'), 'Translations', @language_id);
+INSERT INTO `{tbl_prefix}languages_translations` (`id_language_key`, `translation`, `language_id`)
+VALUES ((SELECT id_language_key FROM `{tbl_prefix}languages_keys` WHERE `language_key` = 'partially_specified'), 'Partially specified', @language_id);
